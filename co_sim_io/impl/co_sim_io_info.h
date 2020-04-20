5c69d6e8d520d685b3d1eb5ddef2dd1bd77be940
philbucher <philipp.bucher@tum.de>
2020-04-20 22:07:38 +0200

    first draft for CoSimIO Info

---
+++ co_sim_io/impl/co_sim_io_info.h
@@ -0,0 +1,137 @@
+// Kratos   ______     _____ _           ________
+//         / ____/___ / ___/(_)___ ___  /  _/ __ |
+//        / /   / __ \\__ \/ / __ `__ \ / // / / /
+//       / /___/ /_/ /__/ / / / / / / // // /_/ /
+//       \____/\____/____/_/_/ /_/ /_/___/\____/
+//      Kratos CoSimulationApplication
+//
+//  License:         BSD License, see license.txt
+//
+//  Main authors:    Philipp Bucher (https://github.com/philbucher)
+//
+
+#ifndef KRATOS_CO_SIM_IO_INFO_H_INCLUDED
+#define KRATOS_CO_SIM_IO_INFO_H_INCLUDED
+
+// System includes
+#include <string>
+#include <unordered_map>
+#include <typeinfo>
+#include <utility>
+
+#ifdef CO_SIM_IO_USING_MPI
+#include "mpi.h"
+#endif // CO_SIM_IO_USING_MPI
+
+namespace CoSimIO {
+
+// class Info
+// {
+// public:
+//     virtual ~Info() = default;
+
+//     template<typename TDataType>
+//     TDataType GetValue(const std::string& rKey)
+//     {
+//         KRATOS_CO_SIM_ERROR_IF_NOT(HasValue<TDataType>(rKey)) << "Does not exist" << std::endl;
+//         KRATOS_CO_SIM_ERROR_IF_NOT(mOptions[rKey].first(), typeid(TDataType)) << "Wrong DataType!";
+
+//         return static_cast<TDataType>(mOptions[rKey]);
+//     }
+
+//     template<typename TDataType>
+//     bool HasValue(const std::string& rKey) const
+//     {
+//         const bool has_value = mOptions.count(rKey)>0;
+//         if (has_value) {
+//             KRATOS_CO_SIM_ERROR_IF_NOT(mOptions[rKey].first(), typeid(TDataType)) << "Wrong DataType!";
+//         }
+
+//         return has_value;
+//     }
+
+//     template<typename TDataType>
+//     void SetValue(const std::string& rKey, const TDataType Value)
+//     {
+//         static_assert(
+//             std::is_same<TDataType, double>::value ||
+//             std::is_same<TDataType, int>::value    ||
+//             std::is_same<TDataType, bool>::value   ||
+//             std::is_same<TDataType, std::string>::value,
+//                 "Only allowed types are double, int, bool, string");
+
+//         mOptions[rKey] = {typeid(TDataType), Value};
+//     }
+
+// private:
+//     std::unordered_map<std::string , std::pair<std::type_info, void*>> mOptions;
+// };
+
+
+// class ConnectionSettings : Info
+// {
+// public:
+//     int GetEchoLevel() const {return mEchoLevel;}
+//     void SetEchoLevel(const int EchoLevel) {mEchoLevel = EchoLevel;}
+
+//     int GetNumberOfProcessors() const {return mNumberOfProcessors;}
+//     void SetNumberOfProcessors(const int NumberOfProcessors) {mNumberOfProcessors = NumberOfProcessors;}
+
+//     #ifdef CO_SIM_IO_USING_MPI
+//     MPI_Comm GetMpiComm() const {return mMpiComm;}
+//     void SetMpiComm(const MPI_Comm MpiComm) {mMpiComm = MpiComm;}
+//     #endif // CO_SIM_IO_USING_MPI
+
+// private:
+//     int mEchoLevel = 0;
+//     int mNumberOfProcessors = 1;
+
+//     #ifdef CO_SIM_IO_USING_MPI
+//     MPI_Comm mMpiComm = nullptr;
+//     #endif // CO_SIM_IO_USING_MPI
+// };
+
+
+// struct TransferInfoC
+// {
+//     TransferInfoC(const char* rIdentifier) : Identifier(rIdentifier) {}
+
+//     std::string Identifier;
+// };
+
+// struct TransferInfo
+// {
+//     TransferInfo(const TransferInfoC& rCInfo) {/*...*/} // conversion happens IMPLICITLY!
+//     TransferInfo(const std::string& rIdentifier) : Identifier(rIdentifier) {}
+
+//     std::string Identifier;
+// };
+
+// struct MeshInfo
+// {
+
+// };
+
+
+
+// struct SolutionInfo
+// {
+//     SolutionInfo() {}
+
+//     double CurrentTime;
+//     double NewTime;
+//     bool IsConverged=true;
+
+//     std::unordered_map<std::string, int> MeshStates; // 0=Unchanged // 1=NodesMoved // 2=FullRemesh
+
+// };
+
+
+struct ReturnInfo
+{
+    int ReturnCode;
+};
+
+} // namespace CoSimIO
+
+#endif // KRATOS_CO_SIM_IO_INFO_H_INCLUDED
