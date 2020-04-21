9eeffb30536a8257683c14f7107d0eee0408fb51
Unknown <philipp.bucher@tum.de>
2020-04-22 01:57:45 +0200

    Improvements for Info

--- co_sim_io/impl/co_sim_io_info.h
+++ co_sim_io/impl/co_sim_io_info.h
@@ -25,71 +25,78 @@
 
 namespace CoSimIO {
 
-// class Info
-// {
-// public:
-//     virtual ~Info() = default;
+class Info
+{
+public:
+    virtual ~Info() = default;
 
-//     template<typename TDataType>
-//     TDataType GetValue(const std::string& rKey)
-//     {
-//         KRATOS_CO_SIM_ERROR_IF_NOT(HasValue<TDataType>(rKey)) << "Does not exist" << std::endl;
-//         KRATOS_CO_SIM_ERROR_IF_NOT(mOptions[rKey].first(), typeid(TDataType)) << "Wrong DataType!";
-
-//         return static_cast<TDataType>(mOptions[rKey]);
-//     }
-
-//     template<typename TDataType>
-//     bool HasValue(const std::string& rKey) const
-//     {
-//         const bool has_value = mOptions.count(rKey)>0;
-//         if (has_value) {
-//             KRATOS_CO_SIM_ERROR_IF_NOT(mOptions[rKey].first(), typeid(TDataType)) << "Wrong DataType!";
-//         }
-
-//         return has_value;
-//     }
-
-//     template<typename TDataType>
-//     void SetValue(const std::string& rKey, const TDataType Value)
-//     {
-//         static_assert(
-//             std::is_same<TDataType, double>::value ||
-//             std::is_same<TDataType, int>::value    ||
-//             std::is_same<TDataType, bool>::value   ||
-//             std::is_same<TDataType, std::string>::value,
-//                 "Only allowed types are double, int, bool, string");
-
-//         mOptions[rKey] = {typeid(TDataType), Value};
-//     }
-
-// private:
-//     std::unordered_map<std::string , std::pair<std::type_info, void*>> mOptions;
-// };
+    template<typename TDataType>
+    TDataType GetValue(const std::string& rKey) // const ?
+    {
+        const bool key_exists = HasValue<TDataType>(rKey);
+        // KRATOS_ERROR_IF_NOT(mOptions[rKey].first() == typeid(TDataType)) << "Wrong DataType!";
+        if (key_exists) {
+            return TDataType();
+        } else {
+            return static_cast<TDataType>(mOptions[rKey]);
+        }
+    }
 
 
-// class ConnectionSettings : Info
-// {
-// public:
-//     int GetEchoLevel() const {return mEchoLevel;}
-//     void SetEchoLevel(const int EchoLevel) {mEchoLevel = EchoLevel;}
+    template<typename TDataType>
+    bool HasValue(const std::string& rKey) const
+    {
+        const bool key_exists = mOptions.count(rKey)>0;
+        if (key_exists) {
+            KRATOS_CO_SIM_ERROR_IF_NOT(mOptions.at(rKey).first(), typeid(TDataType)) << "Wrong DataType!";
+        }
 
-//     int GetNumberOfProcessors() const {return mNumberOfProcessors;}
-//     void SetNumberOfProcessors(const int NumberOfProcessors) {mNumberOfProcessors = NumberOfProcessors;}
+        return key_exists;
+    }
 
-//     #ifdef CO_SIM_IO_USING_MPI
-//     MPI_Comm GetMpiComm() const {return mMpiComm;}
-//     void SetMpiComm(const MPI_Comm MpiComm) {mMpiComm = MpiComm;}
-//     #endif // CO_SIM_IO_USING_MPI
+    template<typename TDataType>
+    void SetValue(const std::string& rKey, const TDataType Value)
+    {
+        static_assert(
+            std::is_same<TDataType, double>::value ||
+            std::is_same<TDataType, int>::value    ||
+            std::is_same<TDataType, bool>::value   ||
+            // std::is_same<TDataType, Info>::value   || // makes it recursive
+            std::is_same<TDataType, std::string>::value,
+                "Only allowed types are double, int, bool, string");
 
-// private:
-//     int mEchoLevel = 0;
-//     int mNumberOfProcessors = 1;
+        mOptions[rKey] = {typeid(TDataType), Value};
+    }
 
-//     #ifdef CO_SIM_IO_USING_MPI
-//     MPI_Comm mMpiComm = nullptr;
-//     #endif // CO_SIM_IO_USING_MPI
-// };
+private:
+    std::unordered_map<std::string, std::pair<std::type_info, void*>> mOptions;
+};
+
+
+class ConnectionSettings : Info
+{
+public:
+    // TODO set defaults in constructor
+
+    // int GetEchoLevel() const {return mEchoLevel;}
+    // void SetEchoLevel(const int EchoLevel) {mEchoLevel = EchoLevel;}
+
+    // int GetNumberOfProcessors() const {return mNumberOfProcessors;}
+    // void SetNumberOfProcessors(const int NumberOfProcessors) {mNumberOfProcessors = NumberOfProcessors;}
+
+    #ifdef CO_SIM_IO_USING_MPI
+    MPI_Comm GetMpiComm() const {return mMpiComm;}
+    void SetMpiComm(const MPI_Comm MpiComm) {mMpiComm = MpiComm;}
+    #endif // CO_SIM_IO_USING_MPI
+
+private:
+    // int mEchoLevel = 0;
+    // int mNumberOfProcessors = 1;
+
+    #ifdef CO_SIM_IO_USING_MPI
+    MPI_Comm mMpiComm = nullptr;
+    #endif // CO_SIM_IO_USING_MPI
+};
 
 
 // struct TransferInfoC
