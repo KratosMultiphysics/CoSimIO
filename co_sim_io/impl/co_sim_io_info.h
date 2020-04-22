17a5202b01f02ac9f9810dfb30404c8fedf326b8
Philipp Bucher <philipp.bucher@tum.de>
2020-04-22 19:07:37 +0200

    improvements in Info

--- co_sim_io/impl/co_sim_io_info.h
+++ co_sim_io/impl/co_sim_io_info.h
@@ -31,31 +31,25 @@ public:
     virtual ~Info() = default;
 
     template<typename TDataType>
-    TDataType GetValue(const std::string& rKey) // const ?
+    TDataType Get(const std::string& I_Key) const
     {
-        const bool key_exists = HasValue<TDataType>(rKey);
-        // KRATOS_ERROR_IF_NOT(mOptions[rKey].first() == typeid(TDataType)) << "Wrong DataType!";
+        const bool key_exists = Has(I_Key);
         if (key_exists) {
-            return TDataType();
+            const auto& r_val = mOptions.at(I_Key);
+            KRATOS_ERROR_IF_NOT(r_val.first == Name(TDataType())) << "Wrong DataType!";
+            return static_cast<TDataType>(r_val.second);
         } else {
-            return static_cast<TDataType>(mOptions[rKey]);
+            return TDataType();
         }
     }
 
-
-    template<typename TDataType>
-    bool HasValue(const std::string& rKey) const
+    bool Has(const std::string& I_Key) const
     {
-        const bool key_exists = mOptions.count(rKey)>0;
-        if (key_exists) {
-            KRATOS_CO_SIM_ERROR_IF_NOT(mOptions.at(rKey).first(), typeid(TDataType)) << "Wrong DataType!";
-        }
-
-        return key_exists;
+        return mOptions.count(I_Key)>0;
     }
 
     template<typename TDataType>
-    void SetValue(const std::string& rKey, const TDataType Value)
+    void SetValue(const std::string& I_Key, const TDataType I_Value)
     {
         static_assert(
             std::is_same<TDataType, double>::value ||
@@ -65,12 +59,22 @@ public:
             std::is_same<TDataType, std::string>::value,
                 "Only allowed types are double, int, bool, string");
 
-        mOptions[rKey] = {typeid(TDataType), Value};
+        mOptions[I_Key] = {Name(TDataType()), I_Value};
     }
 
+    // TODO do we need "erase", "clear", "size" ?
+
 private:
-    std::unordered_map<std::string, std::pair<std::type_info, void*>> mOptions;
+    // TODO change type_info to std::string, otherwise serialization is not really possible
+    std::unordered_map<std::string, std::pair<std::string, void*>> mOptions;
+
+    std::string Name(int dummy)         {return "int";}
+    std::string Name(double dummy)      {return "double";}
+    std::string Name(bool dummy)        {return "bool";}
+    std::string Name(std::string dummy) {return "string";}
 };
+
+// TODO add ofstream operator
 
 
 class ConnectionSettings : Info
