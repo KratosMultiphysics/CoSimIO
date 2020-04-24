b6e444bb8eb0bcb7e2e7d6a1f54128e58699dbcb
Philipp Bucher <philipp.bucher@tum.de>
2020-04-24 16:59:29 +0200

    test-runners

---
+++ tests/test_runner_c.c
@@ -0,0 +1,80 @@
+//     ______     _____ _           ________
+//    / ____/___ / ___/(_)___ ___  /  _/ __ |
+//   / /   / __ \\__ \/ / __ `__ \ / // / / /
+//  / /___/ /_/ /__/ / / / / / / // // /_/ /
+//  \____/\____/____/_/_/ /_/ /_/___/\____/
+//  Kratos CoSimulationApplication
+//
+//  License:         BSD License, see license.txt
+//
+//  Main authors:    Philipp Bucher (https://github.com/philbucher)
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+// #include "co_sim_io_c.h"
+
+// #define CONNECTION_NAME "tester_C"
+
+// // #define CHECK_TRUE() ...
+// // #define CHECK_FALSE() ...
+// #define CHECK_EQAUL(a, b) ...
+
+
+// int Test_CoSimIO_Info()
+// {
+//     int test_exit_code = 0;
+//     CoSimIO_Info c_info;
+
+//     const int val = CoSimIO_GetInt(c_info, "some_val_non_existant");
+
+//     CHECK_EQAUL(val, 0)
+
+//     return test_exit_code;
+// }
+
+// int Test_IsConnected()
+// {
+//     int test_exit_code = 0;
+
+//     CoSimIO_ConnectionSettings settings;
+
+//     CoSimIO_ReturnInfo c_info = CoSimIO_Connect(CONNECTION_NAME, settings);
+
+//     CHECK_EQAUL(test_exit_code, c_info.GetReturnCode(), 0);
+
+
+//     CoSimIO_ReturnInfo dc_info = CoSimIO_Disconnect(CONNECTION_NAME);
+//     CHECK_EQAUL(test_exit_code, dc_info.GetReturnCode(), 0);
+
+//     return test_exit_code;
+// }
+
+
+
+// void RunTestCase(const int TestId)
+// {
+//     switch (TestId)
+//     ​{
+//     case 1:
+//       exit(Test_IsConnected());
+//       break;
+
+//     case 2:
+//       // statements
+//       break;
+//     default:
+//       // default statements
+//     }
+
+
+
+
+
+// }
+
+int main(int argc, char *argv[])
+{
+    // read testid
+    // RunTestCase(test_id);
+}
