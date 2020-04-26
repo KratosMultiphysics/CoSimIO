5a2c371dca092e19530fdb618f82de5bb6ea715b
philbucher <philipp.bucher@tum.de>
2020-04-27 01:08:04 +0200

    improve cpp test-runner

--- tests/test_runners/test_runner.cpp
+++ tests/test_runners/test_runner.cpp
@@ -1,12 +1,38 @@
+//     ______     _____ _           ________
+//    / ____/___ / ___/(_)___ ___  /  _/ __ |
+//   / /   / __ \\__ \/ / __ `__ \ / // / / /
+//  / /___/ /_/ /__/ / / / / / / // // /_/ /
+//  \____/\____/____/_/_/ /_/ /_/___/\____/
+//  Kratos CoSimulationApplication
+//
+//  License:         BSD License, see license.txt
+//
+//  Main authors:    Philipp Bucher (https://github.com/philbucher)
+//
 
+
+// System includes
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+// Test includes
 #include "interface/interface_tests.hpp"
 #include "solver_integration/connect_disconnect/solver.hpp"
 #include "solver_integration/data_exchange/solver.hpp"
 #include "solver_integration/mesh_exchange/solver.hpp"
 
-int main(int argc, char *argv[])
+
+int main(int argc, char **argv)
 {
-    // read testid
-    // RunTestCase(test_id);
+    // parsing input
+    if (argc != 2) {
+        throw std::runtime_error("One input argument required: Name of the test to execute");
+    }
+
+    const std::string test_name(argv[1]);
+
+    // run the test with the given test name ...
+
     return 0;
 }
