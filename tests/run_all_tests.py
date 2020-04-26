017747f847cf0baa5d79e7e5647f9122c349f356
philbucher <philipp.bucher@tum.de>
2020-04-26 16:53:05 +0200

    structuring tests

---
+++ tests/run_all_tests.py
@@ -0,0 +1,24 @@
+#     ______     _____ _           ________
+#    / ____/___ / ___/(_)___ ___  /  _/ __ |
+#   / /   / __ \\__ \/ / __ `__ \ / // / / /
+#  / /___/ /_/ /__/ / / / / / / // // /_/ /
+#  \____/\____/____/_/_/ /_/ /_/___/\____/
+#  Kratos CoSimulationApplication
+#
+#  License:         BSD License, see license.txt
+#
+#  Main authors:    Philipp Bucher (https://github.com/philbucher)
+#
+
+# python imports
+import unittest, sys, os
+
+if __name__ == '__main__':
+    verbosity = 0
+    if len(sys.argv) == 2: # verbosity lvl was passed
+        verbosity = int(sys.argv[1])
+    loader = unittest.TestLoader()
+    tests = loader.discover(os.path.dirname(__file__)) # automatically discover all tests in this directory
+    testRunner = unittest.runner.TextTestRunner(verbosity=verbosity)
+    result = testRunner.run(tests).wasSuccessful()
+    sys.exit(not result) # returning inverse bcs for sys.exit 1 aka true means error
