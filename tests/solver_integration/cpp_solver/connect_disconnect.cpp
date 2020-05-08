e1742921f3052ea716c41b078835facf7a773d12
pooyan <pooyan@altair.com>
2020-05-08 17:28:13 +0200

    Adding todo comment

--- tests/solver_integration/cpp_solver/connect_disconnect.cpp
+++ tests/solver_integration/cpp_solver/connect_disconnect.cpp
@@ -16,10 +16,11 @@ int main(){
     settings.Set("solver_version", "1.25");
 
     auto return_info = CoSimIO::Connect(settings);
-    COSIMIO_CHECK_EQUAL(return_info.Get<int>("connection_status"), 1);
+    COSIMIO_CHECK_EQUAL(return_info.Get<int>("connection_status"), 1); // TODO: substitue 1 with CoSimIO::Connected
 
     return_info = CoSimIO::Disconnect(settings); // disconnect afterwards
-    COSIMIO_CHECK_EQUAL(return_info.Get<int>("connection_status"), 0);
+    COSIMIO_CHECK_EQUAL(return_info.Get<int>("connection_status"), 0); // TODO: substitue 1 with CoSimIO::Disconnected
+
     
     return 0;
 }
