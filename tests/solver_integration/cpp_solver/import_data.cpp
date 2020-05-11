672181a2bfd34ef72d6d7c15b29ffd1bd93af61a
pooyan <pooyan@altair.com>
2020-05-11 19:42:30 +0200

    Adding headers comments

--- tests/solver_integration/cpp_solver/import_data.cpp
+++ tests/solver_integration/cpp_solver/import_data.cpp
@@ -1,3 +1,15 @@
+//     ______     _____ _           ________
+//    / ____/___ / ___/(_)___ ___  /  _/ __ |
+//   / /   / __ \\__ \/ / __ `__ \ / // / / /
+//  / /___/ /_/ /__/ / / / / / / // // /_/ /
+//  \____/\____/____/_/_/ /_/ /_/___/\____/
+//  Kratos CoSimulationApplication
+//
+//  License:         BSD License, see license.txt
+//
+//  Main authors:    Pooyan Dadvand
+//
+
 // CoSimulation includes
 #include "co_sim_io.hpp"
 
