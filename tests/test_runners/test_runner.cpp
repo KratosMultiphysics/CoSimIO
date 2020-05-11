ae4f01021f9902e8d1ed9dad0da52bd70cd9eea0
pooyan <pooyan@altair.com>
2020-05-11 19:36:34 +0200

    commenting empty includes

--- tests/test_runners/test_runner.cpp
+++ tests/test_runners/test_runner.cpp
@@ -18,9 +18,9 @@
 
 // Test includes
 #include "interface/interface_tests.hpp"
-#include "solver_integration/connect_disconnect/solver.hpp"
-#include "solver_integration/data_exchange/solver.hpp"
-#include "solver_integration/mesh_exchange/solver.hpp"
+// #include "solver_integration/connect_disconnect/solver.hpp"
+// #include "solver_integration/data_exchange/solver.hpp"
+// #include "solver_integration/mesh_exchange/solver.hpp"
 
 
 int main(int argc, char **argv)
