e6824e73a7ca015d0b8d6160b07a8464a35406a8
philbucher <philipp.bucher@tum.de>
2020-05-12 02:57:10 +0200

    refactored tests

--- tests/co_sim_io/python/test_info.py
+++ tests/co_sim_io/python/test_info.py
@@ -14,181 +14,196 @@
 
 # python imports
 import unittest, sys, os
+from abc import ABCMeta, abstractmethod
 
 import CoSimIO
 
-class CoSimIO_Info_Tests(unittest.TestCase):
-    maxDiff = None # to display all the diff
 
-    def test_basics_int(self):
-        info = CoSimIO.Info()
+class CoSimIO_Info_BaseTests(object):
+    class BaseTests(unittest.TestCase, metaclass=ABCMeta):
+        '''wrapping in an extra class to avoid discovery of the base-test
+        see https://stackoverflow.com/a/25695512
+        '''
+        maxDiff = None # to display all the diff
 
-        self.assertFalse(info.Has("echo_level"))
+        @abstractmethod
+        def _CreateInfo(self): pass
 
-        info.SetInt("echo_level", 1)
+        def test_basics_int(self):
+            info = self._CreateInfo()
 
-        self.assertTrue(info.Has("echo_level"))
+            self.assertFalse(info.Has("echo_level"))
 
-        self.assertEqual(info.GetInt("echo_level"), 1)
+            info.SetInt("echo_level", 1)
 
-    def test_basics_double(self):
-        info = CoSimIO.Info()
+            self.assertTrue(info.Has("echo_level"))
 
-        self.assertFalse(info.Has("tolerance"))
+            self.assertEqual(info.GetInt("echo_level"), 1)
 
-        info.SetDouble("tolerance", 1.5)
+        def test_basics_double(self):
+            info = self._CreateInfo()
 
-        self.assertTrue(info.Has("tolerance"))
+            self.assertFalse(info.Has("tolerance"))
 
-        self.assertAlmostEqual(info.GetDouble("tolerance"), 1.5)
+            info.SetDouble("tolerance", 1.5)
 
-    def test_basics_bool(self):
-        info = CoSimIO.Info()
+            self.assertTrue(info.Has("tolerance"))
 
-        self.assertFalse(info.Has("print_sth"))
+            self.assertAlmostEqual(info.GetDouble("tolerance"), 1.5)
 
-        info.SetBool("print_sth", False)
+        def test_basics_bool(self):
+            info = self._CreateInfo()
 
-        self.assertTrue(info.Has("print_sth"))
+            self.assertFalse(info.Has("print_sth"))
 
-        self.assertFalse(info.GetBool("print_sth"))
+            info.SetBool("print_sth", False)
 
-    def test_basics_string(self):
-        info = CoSimIO.Info()
+            self.assertTrue(info.Has("print_sth"))
 
-        self.assertFalse(info.Has("identifier"))
+            self.assertFalse(info.GetBool("print_sth"))
 
-        info.SetString("identifier", "pressure")
+        def test_basics_string(self):
+            info = self._CreateInfo()
 
-        self.assertTrue(info.Has("identifier"))
+            self.assertFalse(info.Has("identifier"))
 
-        self.assertEqual(info.GetString("identifier"), "pressure")
+            info.SetString("identifier", "pressure")
 
-    def test_non_existing_key(self):
-        info = CoSimIO.Info()
+            self.assertTrue(info.Has("identifier"))
 
-        self.assertFalse(info.Has("identifier"))
+            self.assertEqual(info.GetString("identifier"), "pressure")
 
-        with self.assertRaisesRegex(Exception, 'Error: '): # TODO improve this once errors are working better
-            info.GetBool("identifier")
+        def test_non_existing_key(self):
+            info = self._CreateInfo()
 
-    def test_wrong_type(self):
-        info = CoSimIO.Info()
+            self.assertFalse(info.Has("identifier"))
 
-        self.assertFalse(info.Has("identifier"))
+            with self.assertRaisesRegex(Exception, 'Error: '): # TODO improve this once errors are working better
+                info.GetBool("identifier")
 
-        info.SetString("identifier", "pressure")
+        def test_wrong_type(self):
+            info = self._CreateInfo()
 
-        self.assertTrue(info.Has("identifier"))
+            self.assertFalse(info.Has("identifier"))
 
-        with self.assertRaisesRegex(Exception, 'Error: '): # TODO improve this once errors are working better
-            info.GetBool("identifier")
+            info.SetString("identifier", "pressure")
 
-    def test_many_values(self):
-        info = CoSimIO.Info()
+            self.assertTrue(info.Has("identifier"))
 
-        self.assertFalse(info.Has("identifier"))
-        self.assertFalse(info.Has("is_converged"))
-        self.assertFalse(info.Has("tol"))
-        self.assertFalse(info.Has("echo_level"))
+            with self.assertRaisesRegex(Exception, 'Error: '): # TODO improve this once errors are working better
+                info.GetBool("identifier")
 
-        info.SetString("identifier", "velocity_interface")
-        info.SetBool("is_converged", True)
-        info.SetDouble("tol", 0.008)
-        info.SetInt("echo_level", 2)
+        def test_many_values(self):
+            info = self._CreateInfo()
 
-        self.assertTrue(info.Has("identifier"))
-        self.assertTrue(info.Has("is_converged"))
-        self.assertTrue(info.Has("tol"))
-        self.assertTrue(info.Has("echo_level"))
+            self.assertFalse(info.Has("identifier"))
+            self.assertFalse(info.Has("is_converged"))
+            self.assertFalse(info.Has("tol"))
+            self.assertFalse(info.Has("echo_level"))
 
-        self.assertEqual(info.GetString("identifier"), "velocity_interface")
-        self.assertTrue(info.GetBool("is_converged"))
-        self.assertAlmostEqual(info.GetDouble("tol"), 0.008)
-        self.assertEqual(info.GetInt("echo_level"), 2)
+            info.SetString("identifier", "velocity_interface")
+            info.SetBool("is_converged", True)
+            info.SetDouble("tol", 0.008)
+            info.SetInt("echo_level", 2)
 
-    def test_set_alreay_existing(self):
-        info = CoSimIO.Info()
+            self.assertTrue(info.Has("identifier"))
+            self.assertTrue(info.Has("is_converged"))
+            self.assertTrue(info.Has("tol"))
+            self.assertTrue(info.Has("echo_level"))
 
-        self.assertFalse(info.Has("identifier"))
-        info.SetString("identifier", "velocity_interface")
-        self.assertTrue(info.Has("identifier"))
-        self.assertEqual(info.GetString("identifier"), "velocity_interface")
+            self.assertEqual(info.GetString("identifier"), "velocity_interface")
+            self.assertTrue(info.GetBool("is_converged"))
+            self.assertAlmostEqual(info.GetDouble("tol"), 0.008)
+            self.assertEqual(info.GetInt("echo_level"), 2)
 
-        # now overwriting the already existing value
-        info.SetString("identifier", "pressure")
-        self.assertEqual(info.GetString("identifier"), "pressure")
+        def test_set_alreay_existing(self):
+            info = self._CreateInfo()
 
-    def test_set_alreay_existing_different_data_type(self):
-        info = CoSimIO.Info()
+            self.assertFalse(info.Has("identifier"))
+            info.SetString("identifier", "velocity_interface")
+            self.assertTrue(info.Has("identifier"))
+            self.assertEqual(info.GetString("identifier"), "velocity_interface")
 
-        self.assertFalse(info.Has("identifier"))
-        info.SetString("identifier", "velocity_interface")
-        self.assertTrue(info.Has("identifier"))
-        self.assertEqual(info.GetString("identifier"), "velocity_interface")
+            # now overwriting the already existing value
+            info.SetString("identifier", "pressure")
+            self.assertEqual(info.GetString("identifier"), "pressure")
 
-        # now overwriting the already existing value with a different type
-        info.SetInt("identifier", 15)
-        self.assertEqual(info.GetInt("identifier"), 15)
+        def test_set_alreay_existing_different_data_type(self):
+            info = self._CreateInfo()
 
-    def test_size(self):
-        info = CoSimIO.Info()
-        self.assertTrue(info.Size() == 0)
+            self.assertFalse(info.Has("identifier"))
+            info.SetString("identifier", "velocity_interface")
+            self.assertTrue(info.Has("identifier"))
+            self.assertEqual(info.GetString("identifier"), "velocity_interface")
 
-        info.SetString("identifier", "velocity_interface")
-        info.SetBool("is_converged", True)
-        info.SetDouble("tol", 0.008)
-        info.SetInt("echo_level", 2)
+            # now overwriting the already existing value with a different type
+            info.SetInt("identifier", 15)
+            self.assertEqual(info.GetInt("identifier"), 15)
 
-        self.assertEqual(info.Size(), 4)
-        info.SetInt("echo_level", 6)
-        self.assertEqual(info.Size(), 4)
+        def test_size(self):
+            info = self._CreateInfo()
+            self.assertTrue(info.Size() == 0)
 
-    def test_clear(self):
-        info = CoSimIO.Info()
-        self.assertTrue(info.Size() == 0)
+            info.SetString("identifier", "velocity_interface")
+            info.SetBool("is_converged", True)
+            info.SetDouble("tol", 0.008)
+            info.SetInt("echo_level", 2)
 
-        info.SetString("identifier", "velocity_interface")
-        info.SetBool("is_converged", True)
-        info.SetDouble("tol", 0.008)
-        info.SetInt("echo_level", 2)
+            self.assertEqual(info.Size(), 4)
+            info.SetInt("echo_level", 6)
+            self.assertEqual(info.Size(), 4)
 
-        self.assertEqual(info.Size(), 4)
-        info.SetInt("echo_level", 6)
-        self.assertEqual(info.Size(), 4)
+        def test_clear(self):
+            info = self._CreateInfo()
+            self.assertTrue(info.Size() == 0)
 
-        info.Clear()
-        self.assertEqual(info.Size(), 0)
+            info.SetString("identifier", "velocity_interface")
+            info.SetBool("is_converged", True)
+            info.SetDouble("tol", 0.008)
+            info.SetInt("echo_level", 2)
 
-    def test_erase(self):
-        info = CoSimIO.Info()
-        self.assertTrue(info.Size() == 0)
+            self.assertEqual(info.Size(), 4)
+            info.SetInt("echo_level", 6)
+            self.assertEqual(info.Size(), 4)
 
-        info.SetString("identifier", "velocity_interface")
-        self.assertTrue(info.Has("identifier"))
-        self.assertTrue(info.Size() == 1)
+            info.Clear()
+            self.assertEqual(info.Size(), 0)
 
-        info.Erase("identifier")
-        self.assertFalse(info.Has("identifier"))
-        self.assertTrue(info.Size() == 0)
+        def test_erase(self):
+            info = self._CreateInfo()
+            self.assertTrue(info.Size() == 0)
 
-        # erasing non-existing keys does not throw
-        info.Erase("identifier")
-        info.Erase("whatever")
+            info.SetString("identifier", "velocity_interface")
+            self.assertTrue(info.Has("identifier"))
+            self.assertTrue(info.Size() == 1)
 
-    def test_print(self):
-        info = CoSimIO.Info()
+            info.Erase("identifier")
+            self.assertFalse(info.Has("identifier"))
+            self.assertTrue(info.Size() == 0)
 
-        info.SetString("keyword", "awesome")
-        info.SetBool("is_converged", True)
-        info.SetString("keyword", "awesome")
-        info.SetDouble("tol", 0.008)
-        info.SetInt("echo_level", 2)
-        info.SetInt("checking", 22)
+            # erasing non-existing keys does not throw
+            info.Erase("identifier")
+            info.Erase("whatever")
 
-        exp_string = "CoSimIO-Info; containing 5 entries\n  name: checking | value: 22 | type: int\n  name: echo_level | value: 2 | type: int\n  name: is_converged | value: 1 | type: bool\n  name: keyword | value: awesome | type: string\n  name: tol | value: 0.008 | type: double\n"
+        def test_print(self):
+            info = self._CreateInfo()
 
-        self.assertMultiLineEqual(str(info), exp_string)
+            info.SetString("keyword", "awesome")
+            info.SetBool("is_converged", True)
+            info.SetString("keyword", "awesome")
+            info.SetDouble("tol", 0.008)
+            info.SetInt("echo_level", 2)
+            info.SetInt("checking", 22)
+
+            exp_string = "CoSimIO-Info; containing 5 entries\n  name: checking | value: 22 | type: int\n  name: echo_level | value: 2 | type: int\n  name: is_converged | value: 1 | type: bool\n  name: keyword | value: awesome | type: string\n  name: tol | value: 0.008 | type: double\n"
+
+            self.assertMultiLineEqual(str(info), exp_string)
+
+
+class CoSimIO_Test_Info(CoSimIO_Info_BaseTests.BaseTests):
+    def _CreateInfo(self):
+        return CoSimIO.Info()
+
 
 
 if __name__ == '__main__':
