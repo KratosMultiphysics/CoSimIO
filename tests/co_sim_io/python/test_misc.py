336584e9e928426a9ec439c02d5a5d620f720694
philbucher <philipp.bucher@tum.de>
2020-05-13 00:44:56 +0200

    adding __version__

---
+++ tests/co_sim_io/python/test_misc.py
@@ -0,0 +1,28 @@
+#     ______     _____ _           ________
+#    / ____/___ / ___/(_)___ ___  /  _/ __ |
+#   / /   / __ \\__ \/ / __ `__ \ / // / / /
+#  / /___/ /_/ /__/ / / / / / / // // /_/ /
+#  \____/\____/____/_/_/ /_/ /_/___/\____/
+#  Kratos CoSimulationApplication
+#
+#  License:         BSD License, see license.txt
+#
+#  Main authors:    Philipp Bucher (https://github.com/philbucher)
+#
+
+# miscellaneous for the CoSimIO
+
+# python imports
+import unittest
+
+import CoSimIO
+
+
+class CoSimIO_Misc(unittest.TestCase):
+
+    def test_version(self):
+        self.assertTrue(hasattr(CoSimIO, "__version__"))
+
+
+if __name__ == '__main__':
+    unittest.main()
