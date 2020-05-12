ebe0169db57d9d63ac4e4cbefbc3c17bdb05c6a2
pooyan <pooyan@altair.com>
2020-05-12 13:00:44 +0200

    Making check iqual

--- tests/solver_integration/python_solver/hello.py
+++ tests/solver_integration/python_solver/hello.py
@@ -1,12 +1,16 @@
 import CoSimIO
 return_info = CoSimIO.Hello()
 
+def cosimio_check_equal(a, b):
+    assert a is b
+
+
 print(return_info)
 major_version = return_info.GetInt("major_version")
 minor_version = return_info.GetInt("minor_version")
 patch_version = return_info.GetString("patch_version")
 
-assert major_version is 1
-assert minor_version is 0
+cosimio_check_equal(major_version,1)
+cosimio_check_equal(minor_version,0)
 
 print("CoSimIO version", str(major_version)+"."+str(minor_version)+"."+patch_version)
