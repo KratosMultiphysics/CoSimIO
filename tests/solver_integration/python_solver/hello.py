d79a847e7e8e4620b9a08371529bc6cc9a67366c
pooyan <pooyan@altair.com>
2020-05-12 12:16:42 +0200

    Adding asserts

--- tests/solver_integration/python_solver/hello.py
+++ tests/solver_integration/python_solver/hello.py
@@ -2,9 +2,11 @@ import CoSimIO
 return_info = CoSimIO.Hello()
 
 print(return_info)
-
 major_version = return_info.GetInt("major_version")
 minor_version = return_info.GetInt("minor_version")
 patch_version = return_info.GetString("patch_version")
+
+assert major_version is 1
+assert minor_version is 0
 
 print("CoSimIO version", str(major_version)+"."+str(minor_version)+"."+patch_version)
