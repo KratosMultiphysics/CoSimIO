e3cab83b38110b8129525da43cdabf4e4476ea90
pooyan <pooyan@altair.com>
2020-05-13 17:06:17 +0200

    Freeing the Info

--- tests/solver_integration/c/connect_disconnect.c
+++ tests/solver_integration/c/connect_disconnect.c
@@ -21,18 +21,29 @@
 
 int main()
 {
-    CoSimIO_Info settings;
+    CoSimIO_Info settings=CoSimIO_CreateInfo();
     CoSimIO_Info_SetString(settings, "connection_name", "test_connection"); // The connection name must be unique for each connection between two solvers
     CoSimIO_Info_SetString(settings, "solver_name", "my_solver"); // Not to be confused with the connection name.
     CoSimIO_Info_SetInt(settings, "echo_level", 1);
     CoSimIO_Info_SetString(settings, "solver_version", "1.25");
 
+    // The connect must be called before any CosimIO function called
     CoSimIO_Info info = CoSimIO_Connect(settings);
 
     COSIMIO_CHECK_EQUAL(CoSimIO_Info_GetInt(info, "connection_status"), CoSimIO_Connected);
 
+    // Now you may call any CoSimIO functions 
+
+    // ...
+
+    // You should free the info before getting a new one
+    CoSimIO_FreeInfo(info);
     info = CoSimIO_Disconnect(settings); // disconnect afterwards
     COSIMIO_CHECK_EQUAL(CoSimIO_Info_GetInt(info, "connection_status"), CoSimIO_Disconnected);
+
+    // Don't forget to release the settings and info
+    CoSimIO_FreeInfo(settings);
+    CoSimIO_FreeInfo(info);
 
     return 0;
 }
