4e72390fe0de298e8542c1926a64b0f746da255a
philbucher <philipp.bucher@tum.de>
2020-05-14 16:02:48 +0200

    adding empty tests

---
+++ tests/solver_integration/python_solver/run_tests.py
@@ -0,0 +1,34 @@
+#     ______     _____ _           ________
+#    / ____/___ / ___/(_)___ ___  /  _/ __ |
+#   / /   / __ \\__ \/ / __ `__ \ / // / / /
+#  / /___/ /_/ /__/ / / / / / / // // /_/ /
+#  \____/\____/____/_/_/ /_/ /_/___/\____/
+#  Kratos CoSimulationApplication
+#
+#  License:         BSD License, see license.txt
+#
+#  Main authors:    Philipp Bucher (https://github.com/philbucher)
+#
+
+# tests for the python interface of the CoSimIO
+
+# python imports
+import unittest, sys, os
+import subprocess
+
+class PythonInterfaceTests(unittest.TestCase):
+    def test_hello(self):
+        pass
+
+    def test_connect_disconnect(self):
+        pass
+
+    def test_import_export_data(self):
+        pass
+
+    def test_import_export_mesh(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
