7790831986ae661a9971cca36bcc6f917f57b612
philbucher <philipp.bucher@tum.de>
2020-05-14 16:58:28 +0200

    adding headers

--- tests/solver_integration/python_solver/connect_disconnect.py
+++ tests/solver_integration/python_solver/connect_disconnect.py
@@ -1,3 +1,15 @@
+#     ______     _____ _           ________
+#    / ____/___ / ___/(_)___ ___  /  _/ __ |
+#   / /   / __ \\__ \/ / __ `__ \ / // / / /
+#  / /___/ /_/ /__/ / / / / / / // // /_/ /
+#  \____/\____/____/_/_/ /_/ /_/___/\____/
+#  Kratos CoSimulationApplication
+#
+#  License:         BSD License, see license.txt
+#
+#  Main authors:    Pooyan Dadvand
+#
+
 import CoSimIO
 
 def cosimio_check_equal(a, b):
@@ -6,7 +18,7 @@ def cosimio_check_equal(a, b):
 
 settings = CoSimIO.Info()
 settings.SetString("connection_name", "test_connection") # This must be unique for each connection between two solvers
-settings.SetString("solver_name", "my_solver") # Not to be confused with the connection name. 
+settings.SetString("solver_name", "my_solver") # Not to be confused with the connection name.
 settings.SetInt("echo_level", 1)
 settings.SetString("solver_version", "1.25")
 
--- tests/solver_integration/python_solver/hello.py
+++ tests/solver_integration/python_solver/hello.py
@@ -1,9 +1,21 @@
+#     ______     _____ _           ________
+#    / ____/___ / ___/(_)___ ___  /  _/ __ |
+#   / /   / __ \\__ \/ / __ `__ \ / // / / /
+#  / /___/ /_/ /__/ / / / / / / // // /_/ /
+#  \____/\____/____/_/_/ /_/ /_/___/\____/
+#  Kratos CoSimulationApplication
+#
+#  License:         BSD License, see license.txt
+#
+#  Main authors:    Pooyan Dadvand
+#
+
 import CoSimIO
-info = CoSimIO.Hello()
 
 def cosimio_check_equal(a, b):
     assert a is b
 
+info = CoSimIO.Hello()
 
 print(info)
 major_version = info.GetInt("major_version")
