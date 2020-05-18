f99a4d3ecd9f8dfd0b849bc1e91c9cb7da73ee19
philbucher <philipp.bucher@tum.de>
2020-05-19 01:20:15 +0200

    import export mesh for C working

--- tests/solver_integration/c/import_mesh.c
+++ tests/solver_integration/c/import_mesh.c
@@ -7,7 +7,7 @@
 //
 //  License:         BSD License, see license.txt
 //
-//  Main authors:    Pooyan Dadvand
+//  Main authors:    Philipp Bucher (https://github.com/philbucher)
 //
 
 // CoSimulation includes
@@ -33,40 +33,27 @@ int main()
     COSIMIO_CHECK_EQUAL(CoSimIO_Info_GetInt(connect_info, "connection_status"), CoSimIO_Connected);
     CoSimIO_FreeInfo(connect_info); // Don't forget to free the connect_info
 
-    int number_of_nodes=6;
-    double nodal_coordinates[] = {
-        0.0, 2.5, 1.0, /*0*/
-        2.0, 0.0, 1.5, /*1*/
-        2.0, 2.5, 1.5, /*2*/
-        4.0, 2.5, 1.7, /*3*/
-        4.0, 0.0, 1.7, /*4*/
-        6.0, 0.0, 1.8  /*5*/
-    };
+    // After conneting we may import the data
+    double* nodal_coordinates;
+    int number_of_nodes = 0;
+    int* elements_connectivities;
+    int number_of_elements_connectivities = 0;
+    int* elements_types;
+    int number_of_elements = 0;
 
-    int number_of_elements_connectivities = 12;
-    int elements_connectivities[] = {
-        0, 1, 2, /*1*/
-        1, 3, 2, /*2*/
-        1, 4, 3, /*3*/
-        3, 4, 5, /*4*/
-    };
+    // Creating the import_settings
+    CoSimIO_Info import_settings=CoSimIO_CreateInfo();
+    CoSimIO_Info_SetString(import_settings, "identifier", "fluid_mesh");
+    CoSimIO_Info_SetString(import_settings, "connection_name", "test_connection");
 
-    int number_of_elements = 4;
-    int elements_types[] = {5,5,5,5}; // VTK_TRIANGLE
+    // Importing the mesh
+    CoSimIO_Info import_info = CoSimIO_ImportMesh(import_settings
+        , &number_of_nodes,&number_of_elements,&number_of_elements_connectivities
+        , &nodal_coordinates, &elements_connectivities, &elements_types);
 
-    // Creatint the export_settings
-    CoSimIO_Info export_settings=CoSimIO_CreateInfo();
-    CoSimIO_Info_SetString(export_settings, "identifier", "fluid_mesh");
-    CoSimIO_Info_SetString(export_settings, "connection_name", "test_connection");
-
-    // Exporting the data
-    CoSimIO_Info export_info = CoSimIO_ExportMesh(export_settings
-        , number_of_nodes,number_of_elements,number_of_elements_connectivities
-        , nodal_coordinates, elements_connectivities, elements_types);
-
-    // Freeing the export_info and export_settings
-    CoSimIO_FreeInfo(export_info);
-    CoSimIO_FreeInfo(export_settings);
+    // Freeing the import_info and import_settings
+    CoSimIO_FreeInfo(import_info);
+    CoSimIO_FreeInfo(import_settings);
 
     // Disconnecting at the end
     CoSimIO_Info disconnect_info = CoSimIO_Disconnect(connection_settings); // disconnect afterwards
