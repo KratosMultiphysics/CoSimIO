2aef91d06e54f0643b2dafca14694ebcf7c0befe
philbucher <philipp.bucher@tum.de>
2020-07-28 15:25:22 +0200

    refactor

--- co_sim_io/impl/info.hpp
+++ co_sim_io/impl/info.hpp
@@ -112,18 +112,14 @@ public:
     const TDataType& Get(const std::string& I_Key) const
     {
         CO_SIM_IO_ERROR_IF_NOT(Has(I_Key)) << "Trying to get \"" << I_Key << "\" which does not exist!" << std::endl;
-        const auto& r_val = mOptions.at(I_Key);
-        CO_SIM_IO_ERROR_IF(r_val->GetDataTypeName() != Internals::Name(TDataType())) << "Wrong DataType! Trying to get \"" << I_Key << "\" which is of type \"" << r_val->GetDataTypeName() << "\" with \"" << Internals::Name(TDataType()) << "\"!" << std::endl;
-        return *static_cast<const TDataType*>(r_val->GetData());
+        return GetExistingKey(I_Key);
     }
 
     template<typename TDataType>
     const TDataType& Get(const std::string& I_Key, const TDataType& I_Default) const
     {
         if (Has(I_Key)) {
-            const auto& r_val = mOptions.at(I_Key);
-            CO_SIM_IO_ERROR_IF(r_val->GetDataTypeName() != Internals::Name(TDataType())) << "Wrong DataType! Trying to get \"" << I_Key << "\" which is of type \"" << r_val->GetDataTypeName() << "\" with \"" << Internals::Name(TDataType()) << "\"!" << std::endl;
-            return *static_cast<const TDataType*>(r_val->GetData());
+            return GetExistingKey(I_Key);
         } else {
             return I_Default;
         }
@@ -225,6 +221,14 @@ public:
 
 private:
     std::map<std::string, std::shared_ptr<Internals::InfoDataBase>> mOptions;
+
+    template<typename TDataType>
+    const TDataType& GetExistingKey(const std::string& I_Key) const
+    {
+        const auto& r_val = mOptions.at(I_Key);
+        CO_SIM_IO_ERROR_IF(r_val->GetDataTypeName() != Internals::Name(TDataType())) << "Wrong DataType! Trying to get \"" << I_Key << "\" which is of type \"" << r_val->GetDataTypeName() << "\" with \"" << Internals::Name(TDataType()) << "\"!" << std::endl;
+        return *static_cast<const TDataType*>(r_val->GetData());
+    }
 };
 
 /// output stream function
