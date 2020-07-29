c17943bfde414d8d5454bdbccd6765e644c23ffa
philbucher <philipp.bucher@tum.de>
2020-07-29 11:55:36 +0200

    adding test files

---
+++ tests/co_sim_io/impl/test_include_co_sim_io_2.cpp
@@ -0,0 +1,17 @@
+//     ______     _____ _           ________
+//    / ____/___ / ___/(_)___ ___  /  _/ __ |
+//   / /   / __ \\__ \/ / __ `__ \ / // / / /
+//  / /___/ /_/ /__/ / / / / / / // // /_/ /
+//  \____/\____/____/_/_/ /_/ /_/___/\____/
+//  Kratos CoSimulationApplication
+//
+//  License:         BSD License, see license.txt
+//
+//  Main authors:    Philipp Bucher (https://github.com/philbucher)
+//
+
+/* see "test_include_co_sim_io_1.cpp" for why this file is necessary
+*/
+
+// Project includes
+#include "co_sim_io.hpp"
