eb50f896d7ce5c80932121f48c8a58ce1902e1bd
philbucher <philipp.bucher@tum.de>
2020-08-27 11:20:40 +0200

    added test for Node

--- tests/co_sim_io/impl/test_model_part.cpp
+++ tests/co_sim_io/impl/test_model_part.cpp
@@ -17,8 +17,37 @@
 
 namespace CoSimIO {
 
-// TEST_SUITE("ModelPart") {
+TEST_SUITE("ModelPart") {
 
-// } // TEST_SUITE("ModelPart")
+TEST_CASE("node")
+{
+    const std::array<double, 3> coords = {1.0, -2.7, 9.44};
+    const int id = 16;
+
+    std::unique_ptr<Node> p_node;
+
+    SUBCASE("from_coords")
+    {
+        p_node = CoSimIO::make_unique<Node>(id, coords[0], coords[1], coords[2]);
+    }
+
+    SUBCASE("from_coords_array")
+    {
+        p_node = CoSimIO::make_unique<Node>(id, coords);
+    }
+
+    CHECK_EQ(p_node->Id(), id);
+
+    CHECK_EQ(p_node->X(), doctest::Approx(coords[0]));
+    CHECK_EQ(p_node->Y(), doctest::Approx(coords[1]));
+    CHECK_EQ(p_node->Z(), doctest::Approx(coords[2]));
+
+    for (std::size_t i=0; i<3; ++i) {
+        CAPTURE(i); // log the current input data
+        CHECK_EQ(p_node->Coordinates()[i], doctest::Approx(coords[i]));
+    }
+}
+
+} // TEST_SUITE("ModelPart")
 
 } // namespace CoSimIO
