affe7c8bba5561f3b27fa5db0aff91581b752805
philbucher <philipp.bucher@tum.de>
2020-08-31 17:45:32 +0200

    missing tests II

--- tests/co_sim_io/impl/test_model_part.cpp
+++ tests/co_sim_io/impl/test_model_part.cpp
@@ -136,7 +136,7 @@ TEST_CASE("model_part_invalid_names")
     CHECK_THROWS_WITH(ModelPart model_part("my_name.ssss"), "Error: "); // TODO find a better way of testing this
 }
 
-TEST_CASE("model_part_add_node")
+TEST_CASE("model_part_create_new_node")
 {
     ModelPart model_part("for_test");
 
@@ -152,7 +152,7 @@ TEST_CASE("model_part_add_node")
     }
 }
 
-TEST_CASE("model_part_add_nodes")
+TEST_CASE("model_part_create_new_nodes")
 {
     ModelPart model_part("for_test");
 
@@ -172,7 +172,7 @@ TEST_CASE("model_part_add_nodes")
     }
 }
 
-TEST_CASE("model_part_add_node_twice")
+TEST_CASE("model_part_create_new_node_twice")
 {
     ModelPart model_part("for_test");
 
@@ -210,77 +210,97 @@ TEST_CASE("model_part_get_node")
     }
 }
 
-TEST_CASE("model_part_add_element")
+TEST_CASE("model_part_create_new_element")
 {
     ModelPart model_part("for_test");
 
     const int node_id = 691;
     const std::array<double, 3> node_coords = {1.0, -2.7, 9.44};
-    const auto& new_node = model_part.CreateNewNode(node_id, node_coords[0], node_coords[1], node_coords[2]);
+    model_part.CreateNewNode(node_id, node_coords[0], node_coords[1], node_coords[2]);
     CHECK_EQ(model_part.NumberOfNodes(), 1);
 
-    CHECK_EQ(new_node.Id(), node_id);
-    for (std::size_t i=0; i<3; ++i) {
-        CAPTURE(i); // log the current input data (done manually as not fully supported yet by doctest)
-        CHECK_EQ(new_node.Coordinates()[i], doctest::Approx(node_coords[i]));
+    const int elem_id = 47;
+    const std::size_t type = 5;
+
+    const auto& new_elem = model_part.CreateNewElement(elem_id, type, {node_id});
+
+    CHECK_EQ(new_elem.Id(), elem_id);
+    CHECK_EQ(new_elem.Type(), type);
+    REQUIRE_EQ(new_elem.NumberOfNodes(), 1);
+
+    for (auto node_it=new_elem.NodesBegin(); node_it!=new_elem.NodesEnd(); ++node_it) {
+        CHECK_EQ((*node_it)->Id(), node_id);
+        for (std::size_t i=0; i<3; ++i) {
+            CAPTURE(i); // log the current input data (done manually as not fully supported yet by doctest)
+            CHECK_EQ((*node_it)->Coordinates()[i], doctest::Approx(node_coords[i]));
+        }
     }
 }
 
-TEST_CASE("model_part_add_elements")
+TEST_CASE("model_part_create_new_elements")
 {
     ModelPart model_part("for_test");
 
-    const int node_id = 691;
+    const int node_ids[] = {2, 159, 61};
     const std::array<double, 3> node_coords = {1.0, -2.7, 9.44};
-    const auto& new_node = model_part.CreateNewNode(node_id, node_coords[0], node_coords[1], node_coords[2]);
-    CHECK_EQ(model_part.NumberOfNodes(), 1);
+    model_part.CreateNewNode(node_ids[0], node_coords[0], node_coords[1], node_coords[2]);
+    model_part.CreateNewNode(node_ids[1], node_coords[1], node_coords[2], node_coords[0]);
+    model_part.CreateNewNode(node_ids[2], node_coords[2], node_coords[0], node_coords[1]);
 
-    model_part.CreateNewNode(node_id+1, node_coords[0], node_coords[1], node_coords[2]);
-    model_part.CreateNewNode(node_id+2, node_coords[0], node_coords[1], node_coords[2]);
     CHECK_EQ(model_part.NumberOfNodes(), 3);
 
-    CHECK_EQ(new_node.Id(), node_id);
-    for (std::size_t i=0; i<3; ++i) {
-        CAPTURE(i); // log the current input data (done manually as not fully supported yet by doctest)
-        CHECK_EQ(new_node.Coordinates()[i], doctest::Approx(node_coords[i]));
+    const int elem_ids[] = {21, 19, 961};
+    const std::size_t elem_types[] = {5, 5, 9};
+    const std::size_t elem_num_nodes[] = {1,1,2};
+
+    model_part.CreateNewElement(elem_ids[0], elem_types[0], {node_ids[0]});
+    model_part.CreateNewElement(elem_ids[1], elem_types[1], {node_ids[1]});
+    model_part.CreateNewElement(elem_ids[2], elem_types[2], {node_ids[1], node_ids[2]});
+
+    REQUIRE_EQ(model_part.NumberOfElements(), 3);
+
+    std::size_t counter = 0;
+    for (auto elem_it=model_part.ElementsBegin(); elem_it!=model_part.ElementsEnd(); ++elem_it) {
+        CHECK_EQ((*elem_it)->Id(), elem_ids[counter]);
+        CHECK_EQ((*elem_it)->Type(), elem_types[counter]);
+        REQUIRE_EQ((*elem_it)->NumberOfNodes(), elem_num_nodes[counter]);
+        counter++;
     }
 }
 
-TEST_CASE("model_part_add_element_twice")
+TEST_CASE("model_part_create_new_element_twice")
 {
     ModelPart model_part("for_test");
 
     model_part.CreateNewNode(1, 0,0,0);
-    REQUIRE_EQ(model_part.NumberOfNodes(), 1);
+    model_part.CreateNewElement(1, 5, {1});
+    REQUIRE_EQ(model_part.NumberOfElements(), 1);
 
-    CHECK_THROWS_WITH(model_part.CreateNewNode(1, 0,0,0), "Error: "); // TODO find a better way of testing this
+    CHECK_THROWS_WITH(model_part.CreateNewElement(1, 5, {1}), "Error: "); // TODO find a better way of testing this
 }
 
 TEST_CASE("model_part_get_element")
 {
     ModelPart model_part("for_test");
 
-    const int node_id = 691;
-    const std::array<double, 3> node_coords = {1.0, -2.7, 9.44};
+    model_part.CreateNewNode(1, 0,0,0);
 
-    model_part.CreateNewNode(node_id, node_coords[0], node_coords[1], node_coords[2]);
-
-    REQUIRE_EQ(model_part.NumberOfNodes(), 1);
+    const int elem_id = 6;
+    model_part.CreateNewElement(elem_id, 5, {1});
+    REQUIRE_EQ(model_part.NumberOfElements(), 1);
 
     SUBCASE("existing")
     {
-        const auto& r_node = model_part.GetNode(node_id);
+        const auto& r_elem = model_part.GetElement(elem_id);
 
-        CHECK_EQ(r_node.Id(), node_id);
-        for (std::size_t i=0; i<3; ++i) {
-            CAPTURE(i); // log the current input data (done manually as not fully supported yet by doctest)
-            CHECK_EQ(r_node.Coordinates()[i], doctest::Approx(node_coords[i]));
-        }
+        CHECK_EQ(r_elem.Id(), elem_id);
+        CHECK_EQ(r_elem.Type(), 5);
+        CHECK_EQ(r_elem.NumberOfNodes(), 1);
     }
 
     SUBCASE("non_existing")
     {
-        CHECK_THROWS_WITH(model_part.GetNode(node_id+1), "Error: "); // TODO find a better way of testing this
+        CHECK_THROWS_WITH(model_part.GetElement(elem_id+1), "Error: "); // TODO find a better way of testing this
     }
 }
 
