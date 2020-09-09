d9757f8d616626e56ff1ed971635ce81c293ced3
philbucher <philipp.bucher@tum.de>
2020-09-09 11:16:33 +0200

    specify holder type

--- co_sim_io/python/model_part_to_python.hpp
+++ co_sim_io/python/model_part_to_python.hpp
@@ -30,7 +30,7 @@ void AddCoSimIOModelPartToPython(pybind11::module& m)
 {
     namespace py = pybind11;
 
-    py::class_<CoSimIO::Node>(m,"Node")
+    py::class_<CoSimIO::Node, CoSimIO::ModelPart::NodePointerType>(m,"Node")
         .def(py::init<const CoSimIO::IdType, const double, const double, const double>())
         .def(py::init<const CoSimIO::IdType, const CoSimIO::CoordinatesType&>())
         .def("Id", &CoSimIO::Node::Id)
@@ -42,7 +42,7 @@ void AddCoSimIOModelPartToPython(pybind11::module& m)
             { std::stringstream ss; ss << I_Node; return ss.str(); } )
         ;
 
-    py::class_<CoSimIO::Element>(m,"Element")
+    py::class_<CoSimIO::Element, CoSimIO::ModelPart::ElementPointerType>(m,"Element")
         .def(py::init<const CoSimIO::IdType, const CoSimIO::Element::ElementType, const CoSimIO::Element::NodesContainerType&>())
         .def("Id", &CoSimIO::Element::Id)
         .def("Type", &CoSimIO::Element::Type)
@@ -59,10 +59,12 @@ void AddCoSimIOModelPartToPython(pybind11::module& m)
         .def("Name",             &CoSimIO::ModelPart::Name)
         .def("NumberOfNodes",    &CoSimIO::ModelPart::NumberOfNodes)
         .def("NumberOfElements", &CoSimIO::ModelPart::NumberOfElements)
-        .def("CreateNewNode",    &CoSimIO::ModelPart::CreateNewNode)
-        .def("CreateNewElement", &CoSimIO::ModelPart::CreateNewElement)
-        // .def("GetNode",          &CoSimIO::ModelPart::GetNode)
-        // .def("GetElement",       &CoSimIO::ModelPart::GetElement)
+        .def("CreateNewNode",    &CoSimIO::ModelPart::CreateNewNode, py::return_value_policy::reference_internal)
+        .def("CreateNewElement", &CoSimIO::ModelPart::CreateNewElement, py::return_value_policy::reference_internal)
+        .def("GetNode",          [](CoSimIO::ModelPart& I_ModelPart, const CoSimIO::IdType I_Id){
+            return I_ModelPart.pGetNode(I_Id);}, py::return_value_policy::reference_internal)
+        .def("GetElement",       [](CoSimIO::ModelPart& I_ModelPart, const CoSimIO::IdType I_Id){
+            return I_ModelPart.pGetElement(I_Id);}, py::return_value_policy::reference_internal)
         .def_property_readonly("Nodes", [](CoSimIO::ModelPart& I_ModelPart) {
             return py::make_iterator(I_ModelPart.NodesBegin(), I_ModelPart.NodesEnd());
             }, py::keep_alive<0, 1>()) /* Keep vector alive while iterator is used */
