e0092109afeccba28dd497eece33599d353c9c51
philbucher <philipp.bucher@tum.de>
2020-09-09 11:28:44 +0200

    finished tests

--- tests/co_sim_io/python/test_model_part.py
+++ tests/co_sim_io/python/test_model_part.py
@@ -10,222 +10,13 @@
 #  Main authors:    Philipp Bucher (https://github.com/philbucher)
 #
 
-# tests for the python exposure of CoSimIO::Info
+# tests for the python exposure of CoSimIO::ModelPart
 
 # python imports
 import unittest
 
 import CoSimIO
 
-
-'''
-
-
-TEST_CASE("model_part_invalid_names")
-{
-    CHECK_THROWS_WITH(ModelPart model_part(""), "Error: "); // TODO find a better way of testing this
-    CHECK_THROWS_WITH(ModelPart model_part("my_name.ssss"), "Error: "); // TODO find a better way of testing this
-}
-
-TEST_CASE("model_part_create_new_node")
-{
-    ModelPart model_part("for_test");
-
-    const int node_id = 691;
-    const std::array<double, 3> node_coords = {1.0, -2.7, 9.44};
-    const auto& new_node = model_part.CreateNewNode(node_id, node_coords[0], node_coords[1], node_coords[2]);
-    CHECK_EQ(model_part.NumberOfNodes(), 1);
-
-    CHECK_EQ(new_node.Id(), node_id);
-    for (std::size_t i=0; i<3; ++i) {
-        CAPTURE(i); // log the current input data (done manually as not fully supported yet by doctest)
-        CHECK_EQ(new_node.Coordinates()[i], doctest::Approx(node_coords[i]));
-    }
-}
-
-TEST_CASE("model_part_create_new_nodes")
-{
-    ModelPart model_part("for_test");
-
-    const int node_id = 691;
-    const std::array<double, 3> node_coords = {1.0, -2.7, 9.44};
-    const auto& new_node = model_part.CreateNewNode(node_id, node_coords[0], node_coords[1], node_coords[2]);
-    CHECK_EQ(model_part.NumberOfNodes(), 1);
-
-    model_part.CreateNewNode(node_id+1, node_coords[0], node_coords[1], node_coords[2]);
-    model_part.CreateNewNode(node_id+2, node_coords[0], node_coords[1], node_coords[2]);
-    CHECK_EQ(model_part.NumberOfNodes(), 3);
-
-    CHECK_EQ(new_node.Id(), node_id);
-    for (std::size_t i=0; i<3; ++i) {
-        CAPTURE(i); // log the current input data (done manually as not fully supported yet by doctest)
-        CHECK_EQ(new_node.Coordinates()[i], doctest::Approx(node_coords[i]));
-    }
-}
-
-TEST_CASE("model_part_create_new_node_twice")
-{
-    ModelPart model_part("for_test");
-
-    model_part.CreateNewNode(1, 0,0,0);
-    REQUIRE_EQ(model_part.NumberOfNodes(), 1);
-
-    CHECK_THROWS_WITH(model_part.CreateNewNode(1, 0,0,0), "Error: "); // TODO find a better way of testing this
-}
-
-TEST_CASE("model_part_get_node")
-{
-    ModelPart model_part("for_test");
-
-    const int node_id = 691;
-    const std::array<double, 3> node_coords = {1.0, -2.7, 9.44};
-
-    model_part.CreateNewNode(node_id, node_coords[0], node_coords[1], node_coords[2]);
-
-    REQUIRE_EQ(model_part.NumberOfNodes(), 1);
-
-    SUBCASE("existing")
-    {
-        const auto& r_node = model_part.GetNode(node_id);
-
-        CHECK_EQ(r_node.Id(), node_id);
-        for (std::size_t i=0; i<3; ++i) {
-            CAPTURE(i); // log the current input data (done manually as not fully supported yet by doctest)
-            CHECK_EQ(r_node.Coordinates()[i], doctest::Approx(node_coords[i]));
-        }
-    }
-
-    SUBCASE("non_existing")
-    {
-        CHECK_THROWS_WITH(model_part.GetNode(node_id+1), "Error: "); // TODO find a better way of testing this
-    }
-}
-
-TEST_CASE("model_part_create_new_element")
-{
-    ModelPart model_part("for_test");
-
-    const int node_id = 691;
-    const std::array<double, 3> node_coords = {1.0, -2.7, 9.44};
-    model_part.CreateNewNode(node_id, node_coords[0], node_coords[1], node_coords[2]);
-    CHECK_EQ(model_part.NumberOfNodes(), 1);
-
-    const int elem_id = 47;
-    const std::size_t type = 5;
-
-    const auto& new_elem = model_part.CreateNewElement(elem_id, type, {node_id});
-
-    CHECK_EQ(new_elem.Id(), elem_id);
-    CHECK_EQ(new_elem.Type(), type);
-    REQUIRE_EQ(new_elem.NumberOfNodes(), 1);
-
-    for (auto node_it=new_elem.NodesBegin(); node_it!=new_elem.NodesEnd(); ++node_it) {
-        CHECK_EQ((*node_it)->Id(), node_id);
-        for (std::size_t i=0; i<3; ++i) {
-            CAPTURE(i); // log the current input data (done manually as not fully supported yet by doctest)
-            CHECK_EQ((*node_it)->Coordinates()[i], doctest::Approx(node_coords[i]));
-        }
-    }
-}
-
-TEST_CASE("model_part_create_new_elements")
-{
-    ModelPart model_part("for_test");
-
-    const int node_ids[] = {2, 159, 61};
-    const std::array<double, 3> node_coords = {1.0, -2.7, 9.44};
-    model_part.CreateNewNode(node_ids[0], node_coords[0], node_coords[1], node_coords[2]);
-    model_part.CreateNewNode(node_ids[1], node_coords[1], node_coords[2], node_coords[0]);
-    model_part.CreateNewNode(node_ids[2], node_coords[2], node_coords[0], node_coords[1]);
-
-    CHECK_EQ(model_part.NumberOfNodes(), 3);
-
-    const int elem_ids[] = {21, 19, 961};
-    const std::size_t elem_types[] = {5, 5, 9};
-    const std::size_t elem_num_nodes[] = {1,1,2};
-
-    model_part.CreateNewElement(elem_ids[0], elem_types[0], {node_ids[0]});
-    model_part.CreateNewElement(elem_ids[1], elem_types[1], {node_ids[1]});
-    model_part.CreateNewElement(elem_ids[2], elem_types[2], {node_ids[1], node_ids[2]});
-
-    REQUIRE_EQ(model_part.NumberOfElements(), 3);
-
-    std::size_t counter = 0;
-    for (auto elem_it=model_part.ElementsBegin(); elem_it!=model_part.ElementsEnd(); ++elem_it) {
-        CHECK_EQ((*elem_it)->Id(), elem_ids[counter]);
-        CHECK_EQ((*elem_it)->Type(), elem_types[counter]);
-        REQUIRE_EQ((*elem_it)->NumberOfNodes(), elem_num_nodes[counter]);
-        counter++;
-    }
-}
-
-TEST_CASE("model_part_create_new_element_twice")
-{
-    ModelPart model_part("for_test");
-
-    model_part.CreateNewNode(1, 0,0,0);
-    model_part.CreateNewElement(1, 5, {1});
-    REQUIRE_EQ(model_part.NumberOfElements(), 1);
-
-    CHECK_THROWS_WITH(model_part.CreateNewElement(1, 5, {1}), "Error: "); // TODO find a better way of testing this
-}
-
-TEST_CASE("model_part_get_element")
-{
-    ModelPart model_part("for_test");
-
-    model_part.CreateNewNode(1, 0,0,0);
-
-    const int elem_id = 6;
-    model_part.CreateNewElement(elem_id, 5, {1});
-    REQUIRE_EQ(model_part.NumberOfElements(), 1);
-
-    SUBCASE("existing")
-    {
-        const auto& r_elem = model_part.GetElement(elem_id);
-
-        CHECK_EQ(r_elem.Id(), elem_id);
-        CHECK_EQ(r_elem.Type(), 5);
-        CHECK_EQ(r_elem.NumberOfNodes(), 1);
-    }
-
-    SUBCASE("non_existing")
-    {
-        CHECK_THROWS_WITH(model_part.GetElement(elem_id+1), "Error: "); // TODO find a better way of testing this
-    }
-}
-
-TEST_CASE("model_part_ostream")
-{
-    ModelPart model_part("for_test");
-
-    std::string exp_string;
-
-    SUBCASE("empty")
-    {
-        exp_string = "CoSimIO-ModelPart \"for_test\"\n    Number of Nodes: 0\n    Number of Elements: 0\n";
-    }
-
-    SUBCASE("with_entities")
-    {
-        const int node_ids[] = {2, 159, 61};
-        const std::array<double, 3> node_coords = {1.0, -2.7, 9.44};
-        model_part.CreateNewNode(node_ids[0], node_coords[0], node_coords[1], node_coords[2]);
-        model_part.CreateNewNode(node_ids[1], node_coords[1], node_coords[2], node_coords[0]);
-        model_part.CreateNewNode(node_ids[2], node_coords[2], node_coords[0], node_coords[1]);
-
-        model_part.CreateNewElement(15, 1, {node_ids[0]});
-
-        exp_string = "CoSimIO-ModelPart \"for_test\"\n    Number of Nodes: 3\n    Number of Elements: 1\n";
-    }
-
-    std::stringstream test_stream;
-
-    test_stream << model_part;
-
-    CHECK_EQ(test_stream.str(), exp_string);
-}
-'''
 
 class CoSimIO_ModelPart(unittest.TestCase):
     maxDiff = None # to display all the diff
@@ -234,19 +25,23 @@ class CoSimIO_ModelPart(unittest.TestCase):
         coords = [1.0, -2.7, 9.44]
         node_id = 16
 
+        def Check(node_to_check):
+            self.assertEqual(node_to_check.Id(), node_id)
+            self.assertAlmostEqual(node_to_check.X(), coords[0])
+            self.assertAlmostEqual(node_to_check.Y(), coords[1])
+            self.assertAlmostEqual(node_to_check.Z(), coords[2])
+
+            for i in range(3):
+                self.assertAlmostEqual(coords[i], node_to_check.Coordinates()[i], msg=str(i))
+
         with self.subTest("from_coords"):
             node = CoSimIO.Node(node_id, coords[0], coords[1], coords[2])
+            Check(node)
 
         with self.subTest("from_coords_array"):
             node = CoSimIO.Node(node_id, coords)
+            Check(node)
 
-        self.assertEqual(node.Id(), node_id)
-        self.assertAlmostEqual(node.X(), coords[0])
-        self.assertAlmostEqual(node.Y(), coords[1])
-        self.assertAlmostEqual(node.Z(), coords[2])
-
-        for i in range(3):
-            self.assertAlmostEqual(coords[i], node.Coordinates()[i], msg=str(i))
 
     def test_print_node(self):
         coords = [1.0, -2.7, 9.44]
@@ -290,7 +85,7 @@ class CoSimIO_ModelPart(unittest.TestCase):
         for i, node in enumerate(element.Nodes):
             self.assertEqual(node.Id(), node_ids[i], msg=str(i))
 
-    def test_element_printing(self):
+    def test_print_element(self):
         dummy_coords = [0,0,0]
         node_1 = CoSimIO.Node(1, dummy_coords)
         node_2 = CoSimIO.Node(22, dummy_coords)
@@ -307,6 +102,161 @@ class CoSimIO_ModelPart(unittest.TestCase):
         self.assertEqual(model_part.Name(), "for_test")
         self.assertEqual(model_part.NumberOfNodes(), 0)
         self.assertEqual(model_part.NumberOfElements(), 0)
+
+    def test_model_part_create_new_node(self):
+        model_part = CoSimIO.ModelPart("for_test")
+
+        node_id = 691
+        node_coords = [1.0, -2.7, 9.44]
+        new_node = model_part.CreateNewNode(node_id, node_coords[0], node_coords[1], node_coords[2])
+        self.assertEqual(model_part.NumberOfNodes(), 1)
+
+        self.assertEqual(new_node.Id(), node_id)
+        for i in range(3):
+            self.assertAlmostEqual(new_node.Coordinates()[i], node_coords[i], msg=str(i))
+
+    def test_model_part_create_new_nodes(self):
+        model_part = CoSimIO.ModelPart("for_test")
+
+        node_id = 691
+        node_coords = [1.0, -2.7, 9.44]
+        new_node = model_part.CreateNewNode(node_id, node_coords[0], node_coords[1], node_coords[2])
+        self.assertEqual(model_part.NumberOfNodes(), 1)
+
+        model_part.CreateNewNode(node_id+1, node_coords[0], node_coords[1], node_coords[2])
+        model_part.CreateNewNode(node_id+2, node_coords[0], node_coords[1], node_coords[2])
+        self.assertEqual(model_part.NumberOfNodes(), 3)
+
+        self.assertEqual(new_node.Id(), node_id)
+        for i in range(3):
+            self.assertAlmostEqual(new_node.Coordinates()[i], node_coords[i], msg=str(i))
+
+        for i, node in enumerate(model_part.Nodes):
+            self.assertEqual(node.Id(), node_id+i, msg=str(i))
+
+    def test_model_part_create_new_node_twice(self):
+        model_part = CoSimIO.ModelPart("for_test")
+
+        model_part.CreateNewNode(1, 0,0,0)
+        self.assertEqual(model_part.NumberOfNodes(), 1)
+
+        with self.assertRaisesRegex(Exception, 'Error: '): # TODO improve this once errors are working better
+            model_part.CreateNewNode(1, 0,0,0)
+
+    def test_model_part_get_node(self):
+        model_part = CoSimIO.ModelPart("for_test")
+
+        node_id = 691
+        node_coords = [1.0, -2.7, 9.44]
+        new_node = model_part.CreateNewNode(node_id, node_coords[0], node_coords[1], node_coords[2])
+        self.assertEqual(model_part.NumberOfNodes(), 1)
+
+        with self.subTest("existing"):
+            node = model_part.GetNode(node_id)
+
+            self.assertEqual(new_node.Id(), node_id)
+            for i in range(3):
+                self.assertAlmostEqual(new_node.Coordinates()[i], node_coords[i], msg=str(i))
+
+        with self.subTest("non_existing"):
+            with self.assertRaisesRegex(Exception, 'Error: '): # TODO improve this once errors are working better
+                model_part.GetNode(node_id+1)
+
+    def test_model_part_create_new_element(self):
+        model_part = CoSimIO.ModelPart("for_test")
+
+        node_id = 691
+        node_coords = [1.0, -2.7, 9.44]
+        new_node = model_part.CreateNewNode(node_id, node_coords[0], node_coords[1], node_coords[2])
+        self.assertEqual(model_part.NumberOfNodes(), 1)
+
+        elem_id = 47
+        elem_type = 5
+
+        new_elem = model_part.CreateNewElement(elem_id, elem_type, [node_id])
+
+        self.assertEqual(new_elem.Id(), elem_id)
+        self.assertEqual(new_elem.Type(), elem_type)
+        self.assertEqual(new_elem.NumberOfNodes(), 1)
+
+        for node in new_elem.Nodes:
+            self.assertEqual(node.Id(), node_id)
+            for i in range(3):
+                self.assertAlmostEqual(node.Coordinates()[i], node_coords[i], msg=str(i))
+
+    def test_model_part_create_new_elements(self):
+        model_part = CoSimIO.ModelPart("for_test")
+
+        node_ids = [2, 159, 61]
+        node_coords = [1.0, -2.7, 9.44]
+        model_part.CreateNewNode(node_ids[0], node_coords[0], node_coords[1], node_coords[2])
+        model_part.CreateNewNode(node_ids[1], node_coords[1], node_coords[2], node_coords[0])
+        model_part.CreateNewNode(node_ids[2], node_coords[2], node_coords[0], node_coords[1])
+
+        self.assertEqual(model_part.NumberOfNodes(), 3)
+
+        elem_ids = [21, 19, 961]
+        elem_types = [5, 5, 9]
+        elem_num_nodes = [1,1,2]
+
+        model_part.CreateNewElement(elem_ids[0], elem_types[0], [node_ids[0]])
+        model_part.CreateNewElement(elem_ids[1], elem_types[1], [node_ids[1]])
+        model_part.CreateNewElement(elem_ids[2], elem_types[2], [node_ids[1], node_ids[2]])
+
+        self.assertEqual(model_part.NumberOfElements(), 3)
+
+        for i, elem in enumerate(model_part.Elements):
+            self.assertEqual(elem.Id(), elem_ids[i])
+            self.assertEqual(elem.Type(), elem_types[i])
+            self.assertEqual(elem.NumberOfNodes(), elem_num_nodes[i])
+
+    def test_model_part_create_new_element_twice(self):
+        model_part = CoSimIO.ModelPart("for_test")
+
+        model_part.CreateNewNode(1, 0,0,0)
+        model_part.CreateNewElement(1, 5, [1])
+        self.assertEqual(model_part.NumberOfElements(), 1)
+
+        with self.assertRaisesRegex(Exception, 'Error: '): # TODO improve this once errors are working better
+            model_part.CreateNewElement(1, 5, [1])
+
+    def test_model_part_get_element(self):
+        model_part = CoSimIO.ModelPart("for_test")
+
+        model_part.CreateNewNode(1, 0,0,0)
+
+        elem_id = 6
+        model_part.CreateNewElement(elem_id, 5, [1])
+        self.assertEqual(model_part.NumberOfElements(), 1)
+
+        with self.subTest("existing"):
+            elem = model_part.GetElement(elem_id)
+            self.assertEqual(elem.Id(), elem_id)
+            self.assertEqual(elem.Type(), 5)
+            self.assertEqual(elem.NumberOfNodes(), 1)
+
+        with self.subTest("non_existing"):
+            with self.assertRaisesRegex(Exception, 'Error: '): # TODO improve this once errors are working better
+                model_part.GetElement(elem_id+1)
+
+    def test_print_model_part(self):
+        model_part = CoSimIO.ModelPart("for_test")
+
+        with self.subTest("empty"):
+            exp_string = "CoSimIO-ModelPart \"for_test\"\n    Number of Nodes: 0\n    Number of Elements: 0\n"
+            self.assertMultiLineEqual(str(model_part), exp_string)
+
+        with self.subTest("with_entities"):
+            node_ids = [2, 159, 61]
+            node_coords = [1.0, -2.7, 9.44]
+            model_part.CreateNewNode(node_ids[0], node_coords[0], node_coords[1], node_coords[2])
+            model_part.CreateNewNode(node_ids[1], node_coords[1], node_coords[2], node_coords[0])
+            model_part.CreateNewNode(node_ids[2], node_coords[2], node_coords[0], node_coords[1])
+
+            model_part.CreateNewElement(15, 1, [node_ids[0]])
+
+            exp_string = "CoSimIO-ModelPart \"for_test\"\n    Number of Nodes: 3\n    Number of Elements: 1\n"
+            self.assertMultiLineEqual(str(model_part), exp_string)
 
 
 if __name__ == '__main__':
