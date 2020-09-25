ae4d630c80adb08b0edcda0fbf7e43e503a4195c
philbucher <philipp.bucher@tum.de>
2020-09-25 16:45:50 +0200

    using different way to set up tests

--- tests/co_sim_io/impl/test_communication.cpp
+++ tests/co_sim_io/impl/test_communication.cpp
@@ -17,33 +17,8 @@
 // Project includes
 #include "co_sim_io_testing.hpp"
 
-// Communication includes
-#include "impl/communication/file_communication.hpp"
-using FileCommunication = CoSimIO::Internals::FileCommunication;
-TYPE_TO_STRING(FileCommunication);
-
-#ifdef CO_SIM_IO_USING_SOCKETS
-#include "impl/communication/sockets_communication.hpp"
-using SocketsCommunication = CoSimIO::Internals::SocketsCommunication;
-TYPE_TO_STRING(SocketsCommunication);
-#endif
-
-#ifdef CO_SIM_IO_USING_MPI
-#include "impl/communication/mpi_communication.hpp"
-using MPICommunication = CoSimIO::Internals::MPICommunication;
-TYPE_TO_STRING(MPICommunication);
-#endif
-
-
-TEST_CASE_TEMPLATE("Communication"* doctest::timeout(0.5), TComm, // neither of the tests should take more than 0.5 seconds. If it does it means that it hangs!
-    FileCommunication
-#ifdef CO_SIM_IO_USING_SOCKETS
-    ,SocketsCommunication
-#endif
-#ifdef CO_SIM_IO_USING_MPI
-    ,MPICommunication
-#endif
-    )
+// neither of the tests should take more than 0.5 seconds. If it does it means that it hangs!
+TEST_CASE_TEMPLATE_DEFINE("Communication"* doctest::timeout(0.5), TCommType, COMM_TESTS)
 {
     SUBCASE("connect_disconnect_once")
     {
@@ -77,6 +52,25 @@ TEST_CASE_TEMPLATE("Communication"* doctest::timeout(0.5), TComm, // neither of 
     {
         // std::this_thread::sleep_for(std::chrono::milliseconds(500)); // wait 0.5s before next check
     }
-
-    // TComm("sss", CoSimIO::Info(), true);
 }
+
+// Registering tests for different types of Communication
+#include "impl/communication/file_communication.hpp"
+using FileCommunication = CoSimIO::Internals::FileCommunication;
+TYPE_TO_STRING(FileCommunication);
+
+TEST_CASE_TEMPLATE_INVOKE(COMM_TESTS, FileCommunication);
+
+#ifdef CO_SIM_IO_USING_SOCKETS
+#include "impl/communication/sockets_communication.hpp"
+using SocketsCommunication = CoSimIO::Internals::SocketsCommunication;
+TYPE_TO_STRING(SocketsCommunication);
+TEST_CASE_TEMPLATE_INVOKE(COMM_TESTS, SocketsCommunication);
+#endif
+
+#ifdef CO_SIM_IO_USING_MPI
+#include "impl/communication/mpi_communication.hpp"
+using MPICommunication = CoSimIO::Internals::MPICommunication;
+TYPE_TO_STRING(MPICommunication);
+TEST_CASE_TEMPLATE_INVOKE(COMM_TESTS, MPICommunication);
+#endif
