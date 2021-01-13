33cb23207e13568addf4115978ec715cede62c0e
Philipp Bucher <philipp.bucher@tum.de>
2021-01-13 09:57:56 +0100

    activate tests in Win (#97)
    * activate tests in Win
    
    * explicitly closing file
    
    * missing ;

--- co_sim_io/impl/communication/file_communication.hpp
+++ co_sim_io/impl/communication/file_communication.hpp
@@ -352,6 +352,7 @@ private:
             }
         }
 
+        input_file.close();
         RemoveFile(file_name);
 
         CO_SIM_IO_INFO_IF("CoSimIO", GetEchoLevel()>1) << "Finished receiving mesh" << std::endl;
