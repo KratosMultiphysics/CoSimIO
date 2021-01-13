3a1d7f448ac7a29c26957c3e2d6d842962252908
philbucher <philipp.bucher@tum.de>
2021-01-13 13:23:54 +0100

    added functionalities for comparing ModelParts

--- tests/co_sim_io/impl/test_communication.cpp
+++ tests/co_sim_io/impl/test_communication.cpp
@@ -20,18 +20,49 @@
 
 namespace {
 
+void CheckNodesAreEqual(
+    const CoSimIO::Node& Node1,
+    const CoSimIO::Node& Node2)
+{
+    CHECK_EQ(Node1.Id(), Node2.Id());
+    CHECK_EQ(Node1.X(), doctest::Approx(Node2.X()));
+    CHECK_EQ(Node1.Y(), doctest::Approx(Node2.Y()));
+    CHECK_EQ(Node1.Z(), doctest::Approx(Node2.Z()));
+}
+
+void CheckElementsAreEqual(
+    const CoSimIO::Element& Element1,
+    const CoSimIO::Element& Element2)
+{
+    // basic checks
+    CHECK_EQ(Element1.Id(), Element2.Id());
+    CHECK_EQ(Element1.Type(), Element2.Type());
+    REQUIRE_EQ(Element1.NumberOfNodes(), Element2.NumberOfNodes());
+
+    // check nodes
+    for (std::size_t i=0; i< Element1.NumberOfNodes(); ++i) {
+        CheckNodesAreEqual(**(Element1.NodesBegin()+i), **(Element2.NodesBegin()+i));
+    }
+}
+
 void CheckModelPartsAreEqual(
     const CoSimIO::ModelPart& ModelPart1,
     const CoSimIO::ModelPart& ModelPart2)
 {
-    // global checks
+    // basic checks
     CHECK_EQ(ModelPart1.Name(), ModelPart2.Name());
-    CHECK_EQ(ModelPart1.NumberOfNodes(), ModelPart2.NumberOfNodes());
-    CHECK_EQ(ModelPart1.NumberOfElements(), ModelPart2.NumberOfElements());
+    REQUIRE_EQ(ModelPart1.NumberOfNodes(), ModelPart2.NumberOfNodes());
+    REQUIRE_EQ(ModelPart1.NumberOfElements(), ModelPart2.NumberOfElements());
 
     // check nodes
+    for (std::size_t i=0; i< ModelPart1.NumberOfNodes(); ++i) {
+        CheckNodesAreEqual(**(ModelPart1.NodesBegin()+i), **(ModelPart2.NodesBegin()+i));
+    }
 
     // check elements
+    for (std::size_t i=0; i< ModelPart1.NumberOfElements(); ++i) {
+        CheckElementsAreEqual(**(ModelPart1.ElementsBegin()+i), **(ModelPart2.ElementsBegin()+i));
+    }
 }
 
 std::shared_ptr<CoSimIO::ModelPart> CreateNodesOnlyModelPart()
