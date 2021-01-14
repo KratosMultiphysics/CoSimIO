ec1200bce355641f28b33dc0c924f2e2eae97acc
philbucher <philipp.bucher@tum.de>
2021-01-14 18:29:33 +0100

    updated remaining C-tests

--- tests/integration_tutorials/c/export_data.c
+++ tests/integration_tutorials/c/export_data.c
@@ -23,37 +23,41 @@ int main()
 {
     // Creating the connection settings
     CoSimIO_Info connection_settings=CoSimIO_CreateInfo();
-    CoSimIO_Info_SetString(connection_settings, "connection_name", "test_connection"); // The connection name must be unique for each connection between two solvers
-    CoSimIO_Info_SetString(connection_settings, "solver_name", "my_solver"); // Not to be confused with the connection name.
+    CoSimIO_Info_SetString(connection_settings, "my_name", "c_export_data");
+    CoSimIO_Info_SetString(connection_settings, "connect_to", "c_import_data");
     CoSimIO_Info_SetInt(connection_settings, "echo_level", 1);
-    CoSimIO_Info_SetString(connection_settings, "solver_version", "1.25");
+    CoSimIO_Info_SetString(connection_settings, "version", "1.25");
 
     // Connecting using the connection settings
     CoSimIO_Info connect_info = CoSimIO_Connect(connection_settings);
     COSIMIO_CHECK_EQUAL(CoSimIO_Info_GetInt(connect_info, "connection_status"), CoSimIO_Connected);
-    CoSimIO_FreeInfo(connect_info); // Don't forget to free the connect_info 
+    const char* connection_name = CoSimIO_Info_GetString(connect_info, "connection_name");
 
     // After conneting we may export the data
     int data_size = 4;
     double data_to_send[] = {3.14, 3.14, 3.14, 3.14};
 
-    // Creatint the export_settings 
+    // Creatint the export_settings
     CoSimIO_Info export_settings=CoSimIO_CreateInfo();
     CoSimIO_Info_SetString(export_settings, "identifier", "vector_of_pi");
-    CoSimIO_Info_SetString(export_settings, "connection_name", "test_connection");
+    CoSimIO_Info_SetString(export_settings, "connection_name", connection_name);
 
     // Exporting the data
     CoSimIO_Info export_info = CoSimIO_ExportData(export_settings, data_size, data_to_send);
     // Freeing the export_info and export_settings
-    CoSimIO_FreeInfo(export_info); 
+    CoSimIO_FreeInfo(export_info);
     CoSimIO_FreeInfo(export_settings);
 
     // Disconnecting at the end
-    CoSimIO_Info disconnect_info = CoSimIO_Disconnect(connection_settings); // disconnect afterwards
+    CoSimIO_Info disconnect_settings=CoSimIO_CreateInfo();
+    CoSimIO_Info_SetString(disconnect_settings, "connection_name", connection_name);
+    CoSimIO_Info disconnect_info = CoSimIO_Disconnect(disconnect_settings); // disconnect afterwards
     COSIMIO_CHECK_EQUAL(CoSimIO_Info_GetInt(disconnect_info, "connection_status"), CoSimIO_Disconnected);
 
     // Don't forget to release the settings and info
     CoSimIO_FreeInfo(connection_settings);
+    CoSimIO_FreeInfo(disconnect_settings);
+    CoSimIO_FreeInfo(connect_info); // Don't forget to free the connect_info
     CoSimIO_FreeInfo(disconnect_info);
 
     return 0;
