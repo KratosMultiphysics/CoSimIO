ab6578492926594140991e38493fda5dacd2d266
philbucher <philipp.bucher@tum.de>
2021-01-18 15:07:59 +0100

    major improvements to Vector exposure

--- co_sim_io/python/vector_to_python.hpp
+++ co_sim_io/python/vector_to_python.hpp
@@ -23,34 +23,61 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-using VEC_DataType = double;
-using VEC_VectorType = std::vector<VEC_DataType>;
+//
+#include "../impl/define.hpp"
 
-PYBIND11_MAKE_OPAQUE(VEC_VectorType)
+// has to be done outside of any namespace!
+PYBIND11_MAKE_OPAQUE(std::vector<int>)
+PYBIND11_MAKE_OPAQUE(std::vector<double>)
 
-void AddCoSimIOVectorToPython(pybind11::module& m)
+namespace {
+
+template<typename TDataType>
+void AddVectorWithTypeToPython(pybind11::module& m, const std::string& Name)
 {
     namespace py = pybind11;
 
-    py::class_<VEC_VectorType>(m,"Vector")
+    using VectorType = std::vector<TDataType>;
+
+    const std::string full_name = Name+"Vector";
+
+    py::class_<VectorType>(m, full_name.c_str())
         .def(py::init<>())
-        .def(py::init<const std::vector<VEC_DataType>&>())
-        .def(py::init<const VEC_VectorType&>())
-        .def("__len__", [](VEC_VectorType& v)
+        .def(py::init<const VectorType&>())
+        .def(py::init( [](const py::list& l){
+            VectorType vec(l.size());
+            for(std::size_t i=0; i<l.size(); ++i) {
+                vec[i] = py::cast<TDataType>(l[i]);
+            }
+            return vec;
+        }))
+        .def(py::init( [Name](py::buffer b){
+            py::buffer_info info = b.request();
+            CO_SIM_IO_ERROR_IF(info.format != py::format_descriptor<TDataType>::value) << "Expected a " << Name << " array!";
+            CO_SIM_IO_ERROR_IF(info.ndim != 1) << "Buffer dimension of 1 is required, got: " << info.ndim << std::endl;
+            VectorType vec(info.shape[0]);
+            for (int i=0; i<info.shape[0]; ++i) {
+                vec[i] = static_cast<TDataType *>(info.ptr)[i];
+            }
+            return vec;
+        }))
+
+        .def("__len__", [](VectorType& v)
             { return v.size(); } )
-        .def("size", [](VEC_VectorType& v)
+        .def("size", [](VectorType& v)
             { return v.size(); } )
-        .def("resize", [](VEC_VectorType& v, const std::size_t& size)
+        .def("resize", [](VectorType& v, const std::size_t& size)
             { v.resize(size); } )
-        .def("append", [](VEC_VectorType& v, const VEC_DataType& val)
+        .def("append", [](VectorType& v, const TDataType& val)
             { v.push_back(val); } )
-        .def("__setitem__", [](VEC_VectorType& v, const std::size_t I_Index, const VEC_DataType& val)
+        .def("__setitem__", [](VectorType& v, const std::size_t I_Index, const TDataType& val)
             { v[I_Index] = val; } )
-        .def("__getitem__", [](VEC_VectorType& v, const std::size_t I_Index)
+        .def("__getitem__", [](VectorType& v, const std::size_t I_Index)
             { return v[I_Index]; } )
-        .def("__iter__",     [](VEC_VectorType& v)
+        .def("__iter__",     [](VectorType& v)
             {return py::make_iterator(v.begin(), v.end());},  py::keep_alive<0,1>())
-        .def("__str__",   [](const VEC_VectorType& v)
+
+        .def("__str__",   [](const VectorType& v)
             {
                 std::stringstream ss;
                 const std::size_t size = v.size();
@@ -65,6 +92,14 @@ void AddCoSimIOVectorToPython(pybind11::module& m)
 
                 return ss.str(); } )
         ;
+}
+
+}
+
+void AddCoSimIOVectorToPython(pybind11::module& m)
+{
+    AddVectorWithTypeToPython<int>(m, "Int");
+    AddVectorWithTypeToPython<double>(m, "Double");
 }
 
 #endif // CO_SIM_IO_VECTOR_TO_PYHON_INCLUDED
--- tests/co_sim_io/python/test_vector.py
+++ tests/co_sim_io/python/test_vector.py
@@ -15,101 +15,168 @@
 
 # python imports
 import unittest
+from abc import ABCMeta, abstractmethod
+
+try:
+    import numpy as np
+    numpy_available = True
+except:
+    numpy_available = False
 
 import CoSimIO
 
 
-class CoSimIO_Vector(unittest.TestCase):
-    maxDiff = None # to display all the diff
+class CoSimIO_Vector:
+    class BaseTests(unittest.TestCase, metaclass=ABCMeta):
+        maxDiff = None # to display all the diff
+        @abstractmethod
+        def _CreateVector(self, *args): pass
 
-    def test_basics(self):
-        vec = CoSimIO.Vector()
-        self.assertEqual(vec.size(), 0)
-        self.assertEqual(len(vec), 0)
+        def test_basics(self):
+            vec = self._CreateVector()
+            self.assertEqual(vec.size(), 0)
+            self.assertEqual(len(vec), 0)
 
-        vec.append(15)
-        self.assertEqual(vec.size(), 1)
-        self.assertEqual(len(vec), 1)
-        self.assertAlmostEqual(vec[0], 15)
+            vec.append(15)
+            self.assertEqual(vec.size(), 1)
+            self.assertEqual(len(vec), 1)
+            self.assertAlmostEqual(vec[0], 15)
 
-        vec.append(-1.45)
-        self.assertEqual(vec.size(), 2)
-        self.assertEqual(len(vec), 2)
-        self.assertAlmostEqual(vec[0], 15)
-        self.assertAlmostEqual(vec[1], -1.45)
+            vec.append(-3)
+            self.assertEqual(vec.size(), 2)
+            self.assertEqual(len(vec), 2)
+            self.assertAlmostEqual(vec[0], 15)
+            self.assertAlmostEqual(vec[1], -3)
 
-    def test_construction_from_objects(self):
-        init_list = [1.0, 2.5, 3.3, -4.1, 17]
+        def test_resize(self):
+            vec = self._CreateVector()
+            self.assertEqual(vec.size(), 0)
 
-        vec_from_list = CoSimIO.Vector(init_list)
-        self.assertEqual(vec_from_list.size(), len(init_list))
+            vec.resize(10)
+            self.assertEqual(vec.size(), 10)
 
-        for l, v in zip(init_list, vec_from_list):
-            self.assertAlmostEqual(l,v)
+            vec.resize(5)
+            self.assertEqual(vec.size(), 5)
 
-    def test_copy_constructor(self):
-        vec = CoSimIO.Vector()
-        for i in range(10):
-            vec.append(i)
+            vec.resize(23)
+            self.assertEqual(vec.size(), 23)
 
-        vec_copy = CoSimIO.Vector(vec)
+        def test_iterators(self):
+            vec = self._CreateVector()
+            for i in range(10):
+                vec.append(i)
 
-        self.assertEqual(vec.size(), vec_copy.size())
-        for v, v_c in zip(vec, vec_copy):
-            self.assertAlmostEqual(v, v_c)
+            self.assertEqual(vec.size(), 10)
 
-        # make sure that it is a deep and not a shallow copy
-        vec[0] = 123456
-        self.assertAlmostEqual(vec_copy[0], 0) # copy is unchanged
+            counter = 0
+            for i, v in enumerate(vec):
+                self.assertAlmostEqual(i,v)
+                counter += 1
+            self.assertEqual(counter, 10)
 
-        vec.resize(5)
-        self.assertEqual(vec_copy.size(), 10)
+            # check for after resize to smaller and larger
+            counter = 0
+            vec.resize(5)
+            for i, v in enumerate(vec):
+                self.assertAlmostEqual(i,v)
+                counter += 1
+            self.assertEqual(counter, 5)
 
-    def test_resize(self):
-        vec = CoSimIO.Vector()
-        self.assertEqual(vec.size(), 0)
+            counter = 0
+            vec.resize(23)
+            for i, v in enumerate(vec):
+                counter += 1
 
-        vec.resize(10)
-        self.assertEqual(vec.size(), 10)
+            self.assertEqual(counter, 23)
 
-        vec.resize(5)
-        self.assertEqual(vec.size(), 5)
+        def test_copy_constructor(self):
+            vec = self._CreateVector()
+            for i in range(10):
+                vec.append(i)
 
-        vec.resize(23)
-        self.assertEqual(vec.size(), 23)
+            vec_copy = self._CreateVector(vec)
 
-    def test_iterators(self):
-        vec = CoSimIO.Vector()
-        for i in range(10):
-            vec.append(i)
+            self.assertEqual(vec.size(), vec_copy.size())
+            for v, v_c in zip(vec, vec_copy):
+                self.assertAlmostEqual(v, v_c)
 
-        self.assertEqual(vec.size(), 10)
+            # make sure that it is a deep and not a shallow copy
+            vec[0] = 123456
+            self.assertAlmostEqual(vec_copy[0], 0) # copy is unchanged
 
-        counter = 0
-        for i, v in enumerate(vec):
-            self.assertAlmostEqual(i,v)
-            counter += 1
-        self.assertEqual(counter, 10)
+            vec.resize(5)
+            self.assertEqual(vec_copy.size(), 10)
 
-        # check for after resize to smaller and larger
-        counter = 0
-        vec.resize(5)
-        for i, v in enumerate(vec):
-            self.assertAlmostEqual(i,v)
-            counter += 1
-        self.assertEqual(counter, 5)
+        def test_construction_from_list(self):
+            init_list = [1, 2, 3, -4, 17]
 
-        counter = 0
-        vec.resize(23)
-        for i, v in enumerate(vec):
-            counter += 1
+            vec = self._CreateVector(init_list)
+            self.assertEqual(vec.size(), len(init_list))
 
-        self.assertEqual(counter, 23)
+            for l, v in zip(init_list, vec):
+                self.assertAlmostEqual(l,v)
+
+
+class CoSimIO_DoubleVector(CoSimIO_Vector.BaseTests):
+    def _CreateVector(self, *args):
+        return CoSimIO.DoubleVector(*args)
 
     def test_print(self):
-        vec = CoSimIO.Vector([1.2, 2.5, 3.3, -4.1, 17])
+        vec = self._CreateVector([1.2, 2.5, 3.3, -4.1, 17])
 
         self.assertMultiLineEqual(str(vec), "[1.2, 2.5, 3.3, -4.1, 17]")
+
+    def test_doubles(self):
+        # general tests are done with ints, hence explicitly checking with doubles
+        vec = self._CreateVector()
+        for i in range(10):
+            vec.append(i*1.5)
+
+        self.assertEqual(vec.size(), 10)
+
+        for i in range(10):
+            self.assertAlmostEqual(vec[i], i*1.5)
+
+    @unittest.skipUnless(numpy_available, "this test requries numpy")
+    def test_construction_from_numpy_array(self):
+        arr = np.array([1.1,2.2,-3.2,4,5,6.78,7,8.78,9], dtype=np.double)
+
+        vec = self._CreateVector(arr)
+        self.assertEqual(vec.size(), arr.size)
+
+        for a, v in zip(arr, vec):
+            self.assertAlmostEqual(a,v)
+
+class CoSimIO_IntVector(CoSimIO_Vector.BaseTests):
+    def _CreateVector(self, *args):
+        return CoSimIO.IntVector(*args)
+
+    def test_print(self):
+        vec = self._CreateVector([1, 2, 3, -4, 17])
+
+        self.assertMultiLineEqual(str(vec), "[1, 2, 3, -4, 17]")
+
+    @unittest.skipUnless(numpy_available, "this test requries numpy")
+    def test_construction_from_numpy_array(self):
+        arr = np.array([10,2,-3,4,5,6,7,8,9], dtype=np.intc)
+
+        vec = self._CreateVector(arr)
+        self.assertEqual(vec.size(), arr.size)
+
+        for a, v in zip(arr, vec):
+            self.assertEqual(a,v)
+
+
+# test with numpy => See Kratos: add_matrix_to_python.cpp => buffer_info
+
+# strcpy for C-connection_name
+
+
+# Paper:
+# - Not restricted to data (should be serializable)
+# - data transer - mapping - orchestrator are separeted => each can be exchnaged independently => can be adapted easily for many usecases
+# - show case
+
 
 
 if __name__ == '__main__':
