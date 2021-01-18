06938545c09dc9ed6f0e850106d4b59721158797
philbucher <philipp.bucher@tum.de>
2021-01-18 10:55:36 +0100

    using PYBIND11_MAKE_OPAQUE

--- co_sim_io/python/vector_to_python.hpp
+++ co_sim_io/python/vector_to_python.hpp
@@ -23,64 +23,43 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-namespace {
+using VEC_DataType = double;
+using VEC_VectorType = std::vector<VEC_DataType>;
 
-// small wrapper around std::vector for using it in Python
-template<typename TDataType>
-class VectorWrapper
-{
-public:
-    VectorWrapper() = default;
-    VectorWrapper(const std::vector<TDataType>& I_Vector)
-        : mVector(I_Vector) {};
-
-    VectorWrapper(const VectorWrapper& Other) : mVector(Other.mVector) {}
-    VectorWrapper& operator=(const VectorWrapper&) = delete;
-
-    std::vector<TDataType>& Vector() {return mVector;}
-    const std::vector<TDataType>& Vector() const {return mVector;}
-
-private:
-    std::vector<TDataType> mVector;
-};
-
-}
+PYBIND11_MAKE_OPAQUE(VEC_VectorType)
 
 void AddCoSimIOVectorToPython(pybind11::module& m)
 {
     namespace py = pybind11;
 
-    using DataType = double;
-    using VectorType = VectorWrapper<DataType>;
-
-    py::class_<VectorType>(m,"Vector")
+    py::class_<VEC_VectorType>(m,"Vector")
         .def(py::init<>())
-        .def(py::init<const std::vector<DataType>&>())
-        .def(py::init<const VectorType&>())
-        .def("__len__", [](VectorType& I_Vec)
-            { return I_Vec.Vector().size(); } )
-        .def("size", [](VectorType& I_Vec)
-            { return I_Vec.Vector().size(); } )
-        .def("resize", [](VectorType& I_Vec, const std::size_t& I_NewSize)
-            { I_Vec.Vector().resize(I_NewSize); } )
-        .def("append", [](VectorType& I_Vec, const DataType& I_Val)
-            { I_Vec.Vector().push_back(I_Val); } )
-        .def("__setitem__", [](VectorType& I_Vec, const std::size_t I_Index, const DataType& I_Val)
-            { I_Vec.Vector()[I_Index] = I_Val; } )
-        .def("__getitem__", [](VectorType& I_Vec, const std::size_t I_Index)
-            { return I_Vec.Vector()[I_Index]; } )
-        .def("__iter__",     [](VectorType& I_Vec)
-            {return py::make_iterator(I_Vec.Vector().begin(), I_Vec.Vector().end());},  py::keep_alive<0,1>())
-        .def("__str__",   [](const VectorType& I_Vec)
+        .def(py::init<const std::vector<VEC_DataType>&>())
+        .def(py::init<const VEC_VectorType&>())
+        .def("__len__", [](VEC_VectorType& v)
+            { return v.size(); } )
+        .def("size", [](VEC_VectorType& v)
+            { return v.size(); } )
+        .def("resize", [](VEC_VectorType& v, const std::size_t& size)
+            { v.resize(size); } )
+        .def("append", [](VEC_VectorType& v, const VEC_DataType& val)
+            { v.push_back(val); } )
+        .def("__setitem__", [](VEC_VectorType& v, const std::size_t I_Index, const VEC_DataType& val)
+            { v[I_Index] = val; } )
+        .def("__getitem__", [](VEC_VectorType& v, const std::size_t I_Index)
+            { return v[I_Index]; } )
+        .def("__iter__",     [](VEC_VectorType& v)
+            {return py::make_iterator(v.begin(), v.end());},  py::keep_alive<0,1>())
+        .def("__str__",   [](const VEC_VectorType& v)
             {
                 std::stringstream ss;
-                const std::size_t size = I_Vec.Vector().size();
+                const std::size_t size = v.size();
 
                 ss << "[";
-                if(size>0) ss << I_Vec.Vector()[0];
+                if(size>0) ss << v[0];
                 if(size>1) {
                     for(std::size_t i=1; i<size; ++i)
-                        ss<<", "<<I_Vec.Vector()[i];
+                        ss<<", "<<v[i];
                 }
                 ss << "]";
 
