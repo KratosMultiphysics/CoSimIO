10bd849e2de19388395bd98ebb0d8c93cb6c30f0
philbucher <philipp.bucher@tum.de>
2021-01-18 15:34:47 +0100

    updated tests

--- tests/integration_tutorials/python/export_mesh.py
+++ tests/integration_tutorials/python/export_mesh.py
@@ -29,30 +29,34 @@ connection_name = return_info.GetString("connection_name")
 
 # Creating the mesh
 nodal_coords = [
-    0.0, 2.5, 1.0, # 0
-    2.0, 0.0, 1.5, # 1
-    2.0, 2.5, 1.5, # 2
-    4.0, 2.5, 1.7, # 3
-    4.0, 0.0, 1.7, # 4
-    6.0, 0.0, 1.8  # 5
+    (0.0, 2.5, 1.0),
+    (2.0, 0.0, 1.5),
+    (2.0, 2.5, 1.5),
+    (4.0, 2.5, 1.7),
+    (4.0, 0.0, 1.7),
+    (6.0, 0.0, 1.8)
 ]
 
 element_connectivities = [
-    0, 1, 2, # 1
-    1, 3, 2, # 2
-    1, 4, 3, # 3
-    3, 4, 5, # 4
+    (1, 2, 3),
+    (2, 4, 3),
+    (2, 5, 4),
+    (4, 5, 6),
 ]
 
-element_types = [
-    5,5,5,5 # VTK_TRIANGLE
-]
+model_part = CoSimIO.ModelPart("mp_exchange")
+
+for i, coords in enumerate(nodal_coords):
+    model_part.CreateNewNode(i+1, *coords)
+
+for i, conn in enumerate(element_connectivities):
+    model_part.CreateNewElement(i+1, CoSimIO.ElementType.TRIANGLE, conn)
 
 # Exporting mesh
 info = CoSimIO.Info()
 info.SetString("identifier", "fluid_mesh")
 info.SetString("connection_name", connection_name)
-return_info = CoSimIO.ExportMesh(info, nodal_coords, element_connectivities, element_types)
+return_info = CoSimIO.ExportMesh(info, model_part)
 
 # Disconnecting
 disconnect_settings = CoSimIO.Info()
