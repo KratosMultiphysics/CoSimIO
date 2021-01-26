9378463e28c70a1c58b1c63636b9c3f5ec469e84
philbucher <philipp.bucher@tum.de>
2021-01-26 10:48:07 +0100

    commenting removal of build dir

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 # should be called with "sh scripts/build.sh" from the root-directory
 
-rm -r build/
+# rm -r build/
 
 cmake -H"." -B"build" \
     -DBUILD_C=ON \
@@ -10,4 +10,3 @@ cmake -H"." -B"build" \
     -DENABLE_MPI=OFF \
     -DCMAKE_BUILD_TYPE=Debug
 cmake --build "build" --target install -- -j$(nproc)
-
--- scripts/build_python.sh
+++ scripts/build_python.sh
@@ -1,6 +1,6 @@
 # should be called with "sh scripts/build_python.sh" from the root-directory
 
-rm -r build/
+# rm -r build/
 
 cmake -H"." -B"build" -DBUILD_PYTHON=ON -DBUILD_TESTING=ON
 cmake --build "build" --target install -- -j$(nproc)
