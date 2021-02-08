e73653179b6476be515302af3ac7a236436e3a70
philbucher <philipp.bucher@tum.de>
2021-02-08 18:21:29 +0100

    missing conversion

--- co_sim_io/impl/communication/communication.hpp
+++ co_sim_io/impl/communication/communication.hpp
@@ -64,7 +64,7 @@ public:
         Info connect_detail_info = ConnectDetail(I_Info);
         mIsConnected = connect_detail_info.Get<bool>("is_connected");
         connect_detail_info.Set<int>("connection_status", ConnectionStatus::Connected);
-        connect_detail_info.Set<std::string>("working_directory", mWorkingDirectory);
+        connect_detail_info.Set<std::string>("working_directory", mWorkingDirectory.string());
 
         CO_SIM_IO_ERROR_IF_NOT(mIsConnected) << "Connection was not successful!" << std::endl;
 
