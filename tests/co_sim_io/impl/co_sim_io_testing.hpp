c08322c702af99e7c10d82f47e1ba0bfaca37480
philbucher <philipp.bucher@tum.de>
2021-03-10 11:03:48 +0100

    move testing function

--- tests/co_sim_io/impl/co_sim_io_testing.hpp
+++ tests/co_sim_io/impl/co_sim_io_testing.hpp
@@ -13,6 +13,9 @@
 // External includes
 #include "doctest/doctest.h"
 
+// Project includes
+#include "impl/model_part.hpp"
+
 // Custom macros
 
 #define CO_SIM_IO_CHECK_VECTOR_NEAR(a, b) {       \
@@ -47,4 +50,49 @@ for (std::size_t _i=0; _i<s; ++_i) {                 \
    CHECK_MESSAGE(a[_i] == b[_i],                     \
    "Mismatch in component: " << _i);                 \
 }                                                    \
+}
+
+inline void CheckNodesAreEqual(
+    const CoSimIO::Node& Node1,
+    const CoSimIO::Node& Node2)
+{
+    CHECK_EQ(Node1.Id(), Node2.Id());
+    CHECK_EQ(Node1.X(), doctest::Approx(Node2.X()));
+    CHECK_EQ(Node1.Y(), doctest::Approx(Node2.Y()));
+    CHECK_EQ(Node1.Z(), doctest::Approx(Node2.Z()));
+}
+
+inline void CheckElementsAreEqual(
+    const CoSimIO::Element& Element1,
+    const CoSimIO::Element& Element2)
+{
+    // basic checks
+    CHECK_EQ(Element1.Id(), Element2.Id());
+    CHECK_EQ(Element1.Type(), Element2.Type());
+    REQUIRE_EQ(Element1.NumberOfNodes(), Element2.NumberOfNodes());
+
+    // check nodes
+    for (std::size_t i=0; i< Element1.NumberOfNodes(); ++i) {
+        CheckNodesAreEqual(**(Element1.NodesBegin()+i), **(Element2.NodesBegin()+i));
+    }
+}
+
+inline void CheckModelPartsAreEqual(
+    const CoSimIO::ModelPart& ModelPart1,
+    const CoSimIO::ModelPart& ModelPart2)
+{
+    // basic checks
+    CHECK_EQ(ModelPart1.Name(), ModelPart2.Name());
+    REQUIRE_EQ(ModelPart1.NumberOfNodes(), ModelPart2.NumberOfNodes());
+    REQUIRE_EQ(ModelPart1.NumberOfElements(), ModelPart2.NumberOfElements());
+
+    // check nodes
+    for (std::size_t i=0; i< ModelPart1.NumberOfNodes(); ++i) {
+        CheckNodesAreEqual(**(ModelPart1.NodesBegin()+i), **(ModelPart2.NodesBegin()+i));
+    }
+
+    // check elements
+    for (std::size_t i=0; i< ModelPart1.NumberOfElements(); ++i) {
+        CheckElementsAreEqual(**(ModelPart1.ElementsBegin()+i), **(ModelPart2.ElementsBegin()+i));
+    }
 }
