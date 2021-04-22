8a3a857899999a31953ccc0ab497f6f0bfed4621
philbucher <philipp.bucher@tum.de>
2021-04-23 00:42:59 +0200

    initial add MPI tests

---
+++ tests/co_sim_io/impl/mpi/main_mpi_test.cpp
@@ -0,0 +1,30 @@
+//     ______     _____ _           ________
+//    / ____/___ / ___/(_)___ ___  /  _/ __ |
+//   / /   / __ \\__ \/ / __ `__ \ / // / / /
+//  / /___/ /_/ /__/ / / / / / / // // /_/ /
+//  \____/\____/____/_/_/ /_/ /_/___/\____/
+//  Kratos CoSimulationApplication
+//
+//  License:         BSD License, see license.txt
+//
+//  Main authors:    Philipp Bucher (https://github.com/philbucher)
+//
+
+#define DOCTEST_CONFIG_IMPLEMENT
+
+#include "doctest/extensions/doctest_mpi.h"
+
+int main(int argc, char** argv) {
+  MPI_Init(&argc, &argv);
+
+  doctest::Context ctx;
+  ctx.setOption("reporters", "MpiConsoleReporter");
+  ctx.setOption("force-colors", true);
+  ctx.applyCommandLine(argc, argv);
+
+  int test_result = ctx.run();
+
+  MPI_Finalize();
+
+  return test_result;
+}
