d03cfbecf839ce08a06cdb7d85d2bd43cd4df105
philbucher <philipp.bucher@tum.de>
2021-04-23 12:20:51 +0200

    adding asio files

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,14 @@
 
 # rm -r build/
 
+# export CXX=icpc
+# export CC=icc
+
 cmake -H"." -B"build" \
     -DBUILD_C=ON \
-    -DBUILD_PYTHON=ON \
+    -DBUILD_PYTHON=OFF \
     -DBUILD_FORTRAN=OFF \
     -DENABLE_SOCKETS=OFF \
-    -DENABLE_MPI=OFF \
-    -DCMAKE_BUILD_TYPE=Debug
-cmake --build "build" --target install -- -j$(nproc)
+    -DENABLE_MPI=ON \
+    -DSTRICT_COMPILER=ON
+cmake --build "build" --target install -- -j6
