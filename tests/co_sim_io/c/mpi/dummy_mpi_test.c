05f100246af10f168183bbf02a718531567ecedc
Philipp <philipp.bucher@tum.de>
2021-05-05 12:31:12 +0200

    try to find valgrind issue

--- tests/co_sim_io/c/mpi/dummy_mpi_test.c
+++ tests/co_sim_io/c/mpi/dummy_mpi_test.c
@@ -20,8 +20,8 @@
 
 int main(int argc, char** argv)
 {
+/*
     int rank;
-
     MPI_Init(&argc, &argv);
 
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
@@ -29,6 +29,6 @@ int main(int argc, char** argv)
     printf("RANK %d", rank);
 
     MPI_Finalize();
-
+*/
     return 0;
 }
