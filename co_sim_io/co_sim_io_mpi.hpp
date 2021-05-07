566a2e03b38271b5f172819c157693e983394dc3
Philipp Bucher <philipp.bucher@tum.de>
2021-05-07 11:13:18 +0200

    [MPI] adding C++ interface (#179)
    * initial add of test files
    
    * initial add of MPI extension
    
    * minor
    
    * using ConnectMPI
    
    * link to MPI if it is used
    hack?
    
    * check if MPI has been initialized
    
    * missing return
    
    * change structure
    
    * update include
    
    * hack is no longer necessary

---
+++ co_sim_io/co_sim_io_mpi.hpp
@@ -0,0 +1,38 @@
+//     ______     _____ _           ________
+//    / ____/___ / ___/(_)___ ___  /  _/ __ |
+//   / /   / __ \\__ \/ / __ `__ \ / // / / /
+//  / /___/ /_/ /__/ / / / / / / // // /_/ /
+//  \____/\____/____/_/_/ /_/ /_/___/\____/
+//  Kratos CoSimulationApplication
+//
+//  License:         BSD License, see license.txt
+//
+//  Main authors:    Philipp Bucher (https://github.com/philbucher)
+//
+
+#ifndef CO_SIM_IO_MPI_INCLUDED
+#define CO_SIM_IO_MPI_INCLUDED
+
+/*
+This file contains the interface of the MPI extension for the CoSimIO
+*/
+
+// System includes
+#include "mpi.h"
+
+// Project includes
+#include "co_sim_io.hpp"
+
+namespace CoSimIO {
+
+
+inline Info ConnectMPI(
+    const Info& I_Settings,
+    MPI_Comm ThisMPIComm);
+
+
+} // namespace CoSimIO
+
+#include "impl/mpi/co_sim_io_mpi_impl.hpp"
+
+#endif // CO_SIM_IO_MPI_INCLUDED
---
+++ co_sim_io/impl/mpi/co_sim_io_mpi_impl.hpp
@@ -0,0 +1,45 @@
+//     ______     _____ _           ________
+//    / ____/___ / ___/(_)___ ___  /  _/ __ |
+//   / /   / __ \\__ \/ / __ `__ \ / // / / /
+//  / /___/ /_/ /__/ / / / / / / // // /_/ /
+//  \____/\____/____/_/_/ /_/ /_/___/\____/
+//  Kratos CoSimulationApplication
+//
+//  License:         BSD License, see license.txt
+//
+//  Main authors:    Philipp Bucher (https://github.com/philbucher)
+//
+
+#ifndef CO_SIM_IO_MPI_IMPL_INCLUDED
+#define CO_SIM_IO_MPI_IMPL_INCLUDED
+
+/*
+This file contains the implementation of the functions defined in "co_sim_io_mpi.hpp"
+*/
+
+// System includes
+#include "mpi.h"
+
+// Project includes
+#include "../define.hpp"
+#include "../info.hpp"
+
+namespace CoSimIO {
+
+inline Info ConnectMPI(
+    const Info& I_Settings,
+    MPI_Comm ThisMPIComm)
+{
+    CO_SIM_IO_ERROR << "Not implemented!" << std::endl;
+
+    // make sure MPI was already initialized by the host
+    int flag_initialized;
+    MPI_Initialized(&flag_initialized);
+    CO_SIM_IO_ERROR_IF_NOT(flag_initialized) << "MPI must be initialized before calling \"ConnectMPI\"!" << std::endl;
+
+    return Info(); // TODO use this
+}
+
+} // namespace CoSimIO
+
+#endif // CO_SIM_IO_MPI_IMPL_INCLUDED
