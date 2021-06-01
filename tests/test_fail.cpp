2592e141f51e6a00cc39acf95432c9e35ef171f0
Philipp Bucher <philipp.bucher@tum.de>
2021-06-01 17:13:47 +0200

    [Testing] fixing return values in run.sh script (#188)
    * adding tests for coupled script
    
    * minor copy paste error
    
    * fixing return value checking

---
+++ tests/test_fail.cpp
@@ -0,0 +1,16 @@
+//     ______     _____ _           ________
+//    / ____/___ / ___/(_)___ ___  /  _/ __ |
+//   / /   / __ \\__ \/ / __ `__ \ / // / / /
+//  / /___/ /_/ /__/ / / / / / / // // /_/ /
+//  \____/\____/____/_/_/ /_/ /_/___/\____/
+//  Kratos CoSimulationApplication
+//
+//  License:         BSD License, see license.txt
+//
+//  Main authors:    Philipp Bucher (https://github.com/philbucher)
+//
+
+int main()
+{
+    return 1;
+}
