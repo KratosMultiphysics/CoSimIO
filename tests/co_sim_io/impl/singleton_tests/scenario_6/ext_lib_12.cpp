4daf91cb611f53f1ae9be1368c2ba23294aae67c
Philipp <philipp.bucher@tum.de>
2021-06-02 15:04:58 +0200

    adding scenario 6

---
+++ tests/co_sim_io/impl/singleton_tests/scenario_6/ext_lib_12.cpp
@@ -0,0 +1,21 @@
+//     ______     _____ _           ________
+//    / ____/___ / ___/(_)___ ___  /  _/ __ |
+//   / /   / __ \\__ \/ / __ `__ \ / // / / /
+//  / /___/ /_/ /__/ / / / / / / // // /_/ /
+//  \____/\____/____/_/_/ /_/ /_/___/\____/
+//  Kratos CoSimulationApplication
+//
+//  License:         BSD License, see license.txt
+//
+//  Main authors:    Philipp Bucher (https://github.com/philbucher)
+//
+
+// Project includes
+#define CO_SIM_IO_EXTERN
+#include "co_sim_io.hpp"
+#include "ext_lib_1.hpp"
+
+bool ExtLib1HasIO(const std::string& rConnectionName)
+{
+    return CoSimIO::Internals::HasIO(rConnectionName);
+}
---
+++ tests/co_sim_io/impl/singleton_tests/scenario_6/ext_lib_2.cpp
@@ -0,0 +1,20 @@
+//     ______     _____ _           ________
+//    / ____/___ / ___/(_)___ ___  /  _/ __ |
+//   / /   / __ \\__ \/ / __ `__ \ / // / / /
+//  / /___/ /_/ /__/ / / / / / / // // /_/ /
+//  \____/\____/____/_/_/ /_/ /_/___/\____/
+//  Kratos CoSimulationApplication
+//
+//  License:         BSD License, see license.txt
+//
+//  Main authors:    Philipp Bucher (https://github.com/philbucher)
+//
+
+// Project includes
+#include "co_sim_io.hpp"
+#include "ext_lib_2.hpp"
+
+bool ExtLib2HasIO(const std::string& rConnectionName)
+{
+    return CoSimIO::Internals::HasIO(rConnectionName);
+}
