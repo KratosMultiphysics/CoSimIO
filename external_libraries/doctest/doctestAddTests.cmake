230a8b8497bdaa9b98019a0cd70d73dbe5c22a98
Philipp Bucher <philipp.bucher@tum.de>
2021-06-24 16:32:21 +0200

    Fix test WORKING_DIRECTORY (#203)
    * adding working dir so that the dlls are found
    
    * pass working dir

--- external_libraries/doctest/doctestAddTests.cmake
+++ external_libraries/doctest/doctestAddTests.cmake
@@ -38,6 +38,7 @@ execute_process(
   COMMAND ${TEST_EXECUTOR} "${TEST_EXECUTABLE}" ${spec} --list-test-cases
   OUTPUT_VARIABLE output
   RESULT_VARIABLE result
+  WORKING_DIRECTORY "${TEST_WORKING_DIR}"
 )
 if(NOT ${result} EQUAL 0)
   message(FATAL_ERROR
