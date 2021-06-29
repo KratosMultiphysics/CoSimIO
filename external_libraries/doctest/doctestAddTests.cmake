ef885793048f3d5a1c4b49b766ee0270a9d1eca9
Philipp <philipp.bucher@tum.de>
2021-06-29 16:04:17 +0200

    making it possible to pass test executor

--- external_libraries/doctest/doctestAddTests.cmake
+++ external_libraries/doctest/doctestAddTests.cmake
@@ -35,7 +35,7 @@ if("${spec}" MATCHES .)
 endif()
 
 execute_process(
-  COMMAND ${TEST_EXECUTOR} "${TEST_EXECUTABLE}" ${spec} --list-test-cases
+  COMMAND "${TEST_EXECUTABLE}" ${spec} --list-test-cases
   OUTPUT_VARIABLE output
   RESULT_VARIABLE result
   WORKING_DIRECTORY "${TEST_WORKING_DIR}"
@@ -68,7 +68,8 @@ foreach(line ${output})
   # ...and add to script
   add_command(add_test
     "${prefix}${test}${suffix}"
-    ${TEST_EXECUTOR}
+    "${TEST_EXECUTOR}"
+    ${TEST_EXECUTOR_ARGS}
     "${TEST_EXECUTABLE}"
     "--test-case=${test_name}"
     "${TEST_JUNIT_OUTPUT_PARAM}"
