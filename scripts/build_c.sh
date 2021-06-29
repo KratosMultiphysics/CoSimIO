6ac97f649136ceef371ad5db2c59c220c1f1c169
Philipp Bucher <philipp.bucher@tum.de>
2021-06-29 11:21:49 +0200

    Drop header only (#202)
    * initial commit
    
    * moved implementation to cpp
    
    * removed impl file
    
    * comment
    
    * include version
    
    * link python
    
    * fix C interface
    
    * add linking for MPI tests
    
    * adding bin to PATH
    
    * more path export
    
    * missing
    
    * try LD_LIBRARY_PATH
    
    * more LD_LIBRARY_PATH
    
    * also use PATH
    
    * try DYLD_LIBRARY_PATH
    
    * adding build dir to PATH in Win
    
    * fix path
    
    * revert adding build dir to path
    
    * msg
    
    * try to set build dir
    
    * full path
    
    * adding message
    
    * adding to path until better fix is found
    
    * removed leftover
    
    * more leftovers removed
    
    * adding build_cpp.sh
    
    * cleanup
    
    * minor
    
    * updated readme
    
    * updated installation docs
    
    * adding comment

--- scripts/build_c.sh
+++ scripts/build_c.sh
@@ -1,6 +1,4 @@
 # should be called with "sh scripts/build_c.sh" from the root-directory
 
-# rm -r build/
-
 cmake -H"." -B"build" -DBUILD_C=ON -DBUILD_TESTING=ON
 cmake --build "build" --target install -- -j$(nproc)
