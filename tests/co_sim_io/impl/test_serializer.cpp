e3982e332148e40651235cdf7d4d6371cab29533
Philipp <philipp.bucher@tum.de>
2021-06-30 18:11:20 +0200

    adding empty test

---
+++ tests/co_sim_io/impl/test_serializer.cpp
@@ -0,0 +1,30 @@
+//     ______     _____ _           ________
+//    / ____/___ / ___/(_)___ ___  /  _/ __ |
+//   / /   / __ \\__ \/ / __ `__ \ / // / / /
+//  / /___/ /_/ /__/ / / / / / / // // /_/ /
+//  \____/\____/____/_/_/ /_/ /_/___/\____/
+//  Kratos CoSimulationApplication
+//
+//  License:         BSD License, see license.txt
+//
+//  Main authors:    Philipp Bucher (https://github.com/philbucher)
+//
+
+// System includes
+#include <sstream>
+
+// Project includes
+#include "co_sim_io_testing.hpp"
+#include "impl/serializer.h"
+#include "impl/file_serializer.h"
+#include "impl/stream_serializer.h"
+
+
+namespace CoSimIO {
+
+TEST_SUITE("Serializer") {
+
+
+} // TEST_SUITE("Serializer")
+
+} // namespace CoSimIO
