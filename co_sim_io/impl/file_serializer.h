07e882ef9989cefadc66548a31764e8841cdf343
Philipp <philipp.bucher@tum.de>
2021-07-02 12:47:03 +0200

    more formatting

--- co_sim_io/impl/file_serializer.h
+++ co_sim_io/impl/file_serializer.h
@@ -15,51 +15,46 @@
 #define CO_SIM_IO_FILE_SERIALIZER_INCLUDED
 
 // System includes
-#include <string>
-#include <cstring>
-#include <iostream>
 #include <iomanip>
-
-// External includes
 
 // Project includes
 #include "serializer.h"
 
 namespace CoSimIO {
 
-//This class provides a simpler interface for serialization to a file
+// This class provides a simpler interface for serialization to a file
 // Note that you may not override any load or save method of the Serializer. They are not virtual
 class FileSerializer : public Serializer
 {
-    public:
-        ///this constructor simply wraps the standard Serializer and defines output to basic_iostream
-        ///@param rTrace type of serialization to be employed
-        FileSerializer(std::string const& Filename, Serializer::TraceType const& rTrace=SERIALIZER_NO_TRACE)
-            : Serializer(nullptr, rTrace)
+  public:
+    ///this constructor simply wraps the standard Serializer and defines output to basic_iostream
+    ///@param rTrace type of serialization to be employed
+    FileSerializer(std::string const& Filename, Serializer::TraceType const& rTrace=SERIALIZER_NO_TRACE)
+        : Serializer(nullptr, rTrace)
+    {
+        std::fstream* p_file = new std::fstream(std::string(Filename+".rest").c_str(), std::ios::binary|std::ios::in|std::ios::out);
+        if(!(*p_file))
         {
-            std::fstream* p_file = new std::fstream(std::string(Filename+".rest").c_str(), std::ios::binary|std::ios::in|std::ios::out);
-            if(!(*p_file))
-            {
-                delete p_file;
-                p_file = new std::fstream(std::string(Filename+".rest").c_str(), std::ios::binary|std::ios::out);
-            }
-            SetBuffer( p_file );
-            CO_SIM_IO_ERROR_IF_NOT(*pGetBuffer()) << "Error opening input file : "
-                                        << std::string(Filename+".rest") << std::endl;
-
-            // increase precision when using ascii
-            if (rTrace != SERIALIZER_NO_TRACE) {
-                *pGetBuffer() << std::setprecision(14);
-            }
+            delete p_file;
+            p_file = new std::fstream(std::string(Filename+".rest").c_str(), std::ios::binary|std::ios::out);
         }
+        SetBuffer( p_file );
+        CO_SIM_IO_ERROR_IF_NOT(*pGetBuffer()) << "Error opening input file : "
+                                    << std::string(Filename+".rest") << std::endl;
 
-        /// Assignment operator.
-        FileSerializer& operator=(FileSerializer const& rOther) = delete;
+        // increase precision when using ascii
+        if (rTrace != SERIALIZER_NO_TRACE) {
+            *pGetBuffer() << std::setprecision(14);
+        }
+    }
 
-        /// Copy constructor.
-        FileSerializer(FileSerializer const& rOther) = delete;
+    /// Assignment operator.
+    FileSerializer& operator=(FileSerializer const& rOther) = delete;
+
+    /// Copy constructor.
+    FileSerializer(FileSerializer const& rOther) = delete;
 };
 
 } // namespace CoSimIO
 
-#endif // CO_SIM_IO_FILE_SERIALIZER_INCLUDED  defined
+#endif // CO_SIM_IO_FILE_SERIALIZER_INCLUDED
--- co_sim_io/impl/stream_serializer.h
+++ co_sim_io/impl/stream_serializer.h
@@ -15,55 +15,50 @@
 #define CO_SIM_IO_STREAM_SERIALIZER_INCLUDED
 
 // System includes
-#include <string>
-#include <cstring>
-#include <iostream>
 #include <iomanip>
-
-// External includes
 
 // Project includes
 #include "serializer.h"
 
-//This class provides a simpler interface for serialization to a string instead of to a file
+// This class provides a simpler interface for serialization to a string instead of to a file
 // Note that you may not override any load or save method of the Serializer. They are not virtual.
 namespace CoSimIO {
 
 class StreamSerializer : public Serializer
 {
-    public:
-        ///this constructor simply wraps the standard Serializer and defines output to basic_iostream
-        ///@param rTrace type of serialization to be employed
-        explicit StreamSerializer(TraceType const& rTrace=SERIALIZER_NO_TRACE)
-            : Serializer(new std::stringstream(std::ios::binary|std::ios::in|std::ios::out),rTrace)
-        {
-            // increase precision when using ascii
-            if (rTrace != SERIALIZER_NO_TRACE) {
-                *pGetBuffer() << std::setprecision(14);
-            }
+  public:
+    ///this constructor simply wraps the standard Serializer and defines output to basic_iostream
+    ///@param rTrace type of serialization to be employed
+    explicit StreamSerializer(TraceType const& rTrace=SERIALIZER_NO_TRACE)
+        : Serializer(new std::stringstream(std::ios::binary|std::ios::in|std::ios::out),rTrace)
+    {
+        // increase precision when using ascii
+        if (rTrace != SERIALIZER_NO_TRACE) {
+            *pGetBuffer() << std::setprecision(14);
         }
+    }
 
-        // //this constructor generates a standard Serializer AND fills the buffer with the data contained in "data"
-        // ///@param data a string contained the data to be used in filling the buffer
-        // ///@param rTrace type of serialization to be employed
-        // StreamSerializer(const std::string& data,TraceType const& rTrace=SERIALIZER_NO_TRACE)
-        //     : Serializer(new std::stringstream(std::ios::binary|std::ios::in|std::ios::out),rTrace)
-        // {
-        //     *(this->pGetBuffer()) << data << std::endl;
-        // }
+    // //this constructor generates a standard Serializer AND fills the buffer with the data contained in "data"
+    // ///@param data a string contained the data to be used in filling the buffer
+    // ///@param rTrace type of serialization to be employed
+    // StreamSerializer(const std::string& data,TraceType const& rTrace=SERIALIZER_NO_TRACE)
+    //     : Serializer(new std::stringstream(std::ios::binary|std::ios::in|std::ios::out),rTrace)
+    // {
+    //     *(this->pGetBuffer()) << data << std::endl;
+    // }
 
-        //get a string representation of the serialized data
-        std::string GetStringRepresentation() {
-            return ((std::stringstream*)(this->pGetBuffer()))->str();
-        }
+    //get a string representation of the serialized data
+    std::string GetStringRepresentation() {
+        return ((std::stringstream*)(this->pGetBuffer()))->str();
+    }
 
-        /// Assignment operator.
-        StreamSerializer& operator=(StreamSerializer const& rOther) = delete;
+    /// Assignment operator.
+    StreamSerializer& operator=(StreamSerializer const& rOther) = delete;
 
-        /// Copy constructor.
-        StreamSerializer(StreamSerializer const& rOther) = delete;
+    /// Copy constructor.
+    StreamSerializer(StreamSerializer const& rOther) = delete;
 };
 
 } // namespace CoSimIO
 
-#endif // CO_SIM_IO_STREAM_SERIALIZER_INCLUDED  defined
+#endif // CO_SIM_IO_STREAM_SERIALIZER_INCLUDED
