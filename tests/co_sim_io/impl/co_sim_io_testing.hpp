430245b017064b004c8840461eeba6cb004d2d03
Philipp <philipp.bucher@tum.de>
2021-07-05 15:10:10 +0200

    split co_sim_io_testing into header/source

--- tests/co_sim_io/impl/co_sim_io_testing.hpp
+++ tests/co_sim_io/impl/co_sim_io_testing.hpp
@@ -10,6 +10,9 @@
 //  Main authors:    Philipp Bucher (https://github.com/philbucher)
 //
 
+#ifndef CO_SIM_IO_TESTING_INCLUDED
+#define CO_SIM_IO_TESTING_INCLUDED
+
 // External includes
 #include "doctest/doctest.h"
 
@@ -52,47 +55,16 @@ for (std::size_t _i=0; _i<(s); ++_i) {               \
 }                                                    \
 }
 
-inline void CheckNodesAreEqual(
+void CheckNodesAreEqual(
     const CoSimIO::Node& Node1,
-    const CoSimIO::Node& Node2)
-{
-    CHECK_EQ(Node1.Id(), Node2.Id());
-    CHECK_EQ(Node1.X(), doctest::Approx(Node2.X()));
-    CHECK_EQ(Node1.Y(), doctest::Approx(Node2.Y()));
-    CHECK_EQ(Node1.Z(), doctest::Approx(Node2.Z()));
-}
+    const CoSimIO::Node& Node2);
 
-inline void CheckElementsAreEqual(
+void CheckElementsAreEqual(
     const CoSimIO::Element& Element1,
-    const CoSimIO::Element& Element2)
-{
-    // basic checks
-    CHECK_EQ(Element1.Id(), Element2.Id());
-    CHECK_EQ(Element1.Type(), Element2.Type());
-    REQUIRE_EQ(Element1.NumberOfNodes(), Element2.NumberOfNodes());
+    const CoSimIO::Element& Element2);
 
-    // check nodes
-    for (std::size_t i=0; i< Element1.NumberOfNodes(); ++i) {
-        CheckNodesAreEqual(**(Element1.NodesBegin()+i), **(Element2.NodesBegin()+i));
-    }
-}
-
-inline void CheckModelPartsAreEqual(
+void CheckModelPartsAreEqual(
     const CoSimIO::ModelPart& ModelPart1,
-    const CoSimIO::ModelPart& ModelPart2)
-{
-    // basic checks
-    CHECK_EQ(ModelPart1.Name(), ModelPart2.Name());
-    REQUIRE_EQ(ModelPart1.NumberOfNodes(), ModelPart2.NumberOfNodes());
-    REQUIRE_EQ(ModelPart1.NumberOfElements(), ModelPart2.NumberOfElements());
+    const CoSimIO::ModelPart& ModelPart2);
 
-    // check nodes
-    for (std::size_t i=0; i< ModelPart1.NumberOfNodes(); ++i) {
-        CheckNodesAreEqual(**(ModelPart1.NodesBegin()+i), **(ModelPart2.NodesBegin()+i));
-    }
-
-    // check elements
-    for (std::size_t i=0; i< ModelPart1.NumberOfElements(); ++i) {
-        CheckElementsAreEqual(**(ModelPart1.ElementsBegin()+i), **(ModelPart2.ElementsBegin()+i));
-    }
-}
+#endif // CO_SIM_IO_UTILITIES_INCLUDED
