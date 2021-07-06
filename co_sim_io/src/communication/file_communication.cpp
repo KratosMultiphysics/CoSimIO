8f14a5f8ab539b615971a34b08dbf59a35542555
Philipp <philipp.bucher@tum.de>
2021-07-06 11:32:49 +0200

    using Serializer in FileComm

--- co_sim_io/src/communication/file_communication.cpp
+++ co_sim_io/src/communication/file_communication.cpp
@@ -23,9 +23,27 @@
 #include "../../impl/communication/file_communication.hpp"
 #include "../../impl/utilities.hpp"
 #include "../../impl/vtk_utilities.hpp"
+#include "../../impl/file_serializer.hpp"
 
 namespace CoSimIO {
 namespace Internals {
+
+namespace {
+
+template<class TObject>
+void SerializeToFile(const fs::path& rPath, const std::string& rTag, const TObject& rObject, const Serializer::TraceType SerializerTrace)
+{
+    FileSerializer serializer(rPath.string(), SerializerTrace);
+    serializer.save(rTag, rObject);
+}
+template<class TObject>
+void SerializeFromFile(const fs::path& rPath, const std::string& rTag, TObject& rObject, const Serializer::TraceType SerializerTrace)
+{
+    FileSerializer serializer(rPath.string(), SerializerTrace);
+    serializer.load(rTag, rObject);
+}
+
+}
 
 FileCommunication::FileCommunication(const Info& I_Settings) : Communication(I_Settings)
 {
@@ -125,18 +143,14 @@ Info FileCommunication::ImportInfoImpl(const Info& I_Info)
 
     WaitForPath(file_name);
 
-    std::ifstream input_file(file_name);
-    CheckStream(input_file, file_name);
+    Info info;
+    SerializeFromFile(file_name, "info", info, Serializer::TraceType::SERIALIZER_NO_TRACE);
 
-    Info imported_info;
-    imported_info.Load(input_file);
-
-    input_file.close(); // TODO check return value?
     RemovePath(file_name);
 
     CO_SIM_IO_INFO_IF("CoSimIO", GetEchoLevel()>1) << "Finished importing Info" << std::endl;
 
-    return imported_info;
+    return info;
 }
 
 Info FileCommunication::ExportInfoImpl(const Info& I_Info)
@@ -150,13 +164,8 @@ Info FileCommunication::ExportInfoImpl(const Info& I_Info)
 
     WaitUntilFileIsRemoved(file_name); // TODO maybe this can be queued somehow ... => then it would not block the sender
 
-    std::ofstream output_file;
-    output_file.open(GetTempFileName(file_name));
-    CheckStream(output_file, file_name);
+    SerializeToFile(GetTempFileName(file_name), "info", I_Info, Serializer::TraceType::SERIALIZER_NO_TRACE);
 
-    I_Info.Save(output_file);
-
-    output_file.close();
     MakeFileVisible(file_name);
 
     CO_SIM_IO_INFO_IF("CoSimIO", GetEchoLevel()>1) << "Finished exporting Info" << std::endl;
@@ -179,24 +188,11 @@ Info FileCommunication::ImportDataImpl(
 
     const auto start_time(std::chrono::steady_clock::now());
 
-    std::ifstream input_file(file_name);
-    CheckStream(input_file, file_name);
+    SerializeFromFile(file_name, "data", rData, Serializer::TraceType::SERIALIZER_NO_TRACE);
 
-    input_file >> std::setprecision(14); // TODO maybe this should be configurable
-
-    int size_read;
-    input_file >> size_read; // the first number in the file is the size of the array
-
-    rData.resize(size_read);
-
-    for (int i=0; i<size_read; ++i) {
-        input_file >> rData[i];
-    }
-
-    input_file.close();
     RemovePath(file_name);
 
-    CO_SIM_IO_INFO_IF("CoSimIO", GetEchoLevel()>1) << "Finished importing array with size: " << size_read << std::endl;
+    CO_SIM_IO_INFO_IF("CoSimIO", GetEchoLevel()>1) << "Finished importing array with size: " << rData.size() << std::endl;
 
     CO_SIM_IO_INFO_IF("CoSimIO", GetPrintTiming()) << "Importing Array \"" << identifier << "\" took: " << ElapsedSeconds(start_time) << " [sec]" << std::endl;
 
@@ -219,21 +215,8 @@ Info FileCommunication::ExportDataImpl(
 
     const auto start_time(std::chrono::steady_clock::now());
 
-    std::ofstream output_file;
-    output_file.open(GetTempFileName(file_name));
-    CheckStream(output_file, file_name);
+    SerializeToFile(GetTempFileName(file_name), "data", rData, Serializer::TraceType::SERIALIZER_NO_TRACE);
 
-    output_file << std::scientific << std::setprecision(14); // TODO maybe this should be configurable
-
-    output_file << size << "\n";
-
-    for (std::size_t i=0; i<size-1; ++i) {
-        output_file << rData[i] << " ";
-    }
-    // TODO check if size == 0!
-    output_file << rData[size-1]; // outside to not have trailing whitespace
-
-    output_file.close();
     MakeFileVisible(file_name);
 
     CO_SIM_IO_INFO_IF("CoSimIO", GetEchoLevel()>1) << "Finished exporting array" << std::endl;
@@ -258,100 +241,8 @@ Info FileCommunication::ImportMeshImpl(
 
     const auto start_time(std::chrono::steady_clock::now());
 
-    std::ifstream input_file(file_name);
-    CheckStream(input_file, file_name);
+    SerializeFromFile(file_name, "model_part", O_ModelPart, Serializer::TraceType::SERIALIZER_NO_TRACE);
 
-    // reading file
-    std::string current_line;
-    std::vector<double> nodal_coords;
-    std::vector<IdType> nodal_ids;
-    std::vector<IdType> element_ids;
-    std::vector<ElementType> element_types;
-    std::vector<ConnectivitiesType> element_connectivities;
-
-    while (std::getline(input_file, current_line)) {
-        // reading nodes
-        if (current_line.find("POINTS") != std::string::npos) {
-            std::size_t num_nodes;
-            current_line = current_line.substr(current_line.find("POINTS") + 7); // removing "POINTS"
-            std::istringstream line_stream(current_line);
-            line_stream >> num_nodes;
-
-            CO_SIM_IO_INFO_IF("CoSimIO", GetEchoLevel()>1) << "Mesh contains " << num_nodes << " Nodes" << std::endl;
-
-            nodal_coords.resize(3*num_nodes);
-            nodal_ids.resize(num_nodes);
-
-            for (std::size_t i=0; i<num_nodes*3; ++i) {
-                input_file >> nodal_coords[i];
-            }
-        }
-
-        // reading connectivities
-        if (current_line.find("CELLS") != std::string::npos) {
-            std::size_t num_elems, num_nodes_per_elem;
-            current_line = current_line.substr(current_line.find("CELLS") + 6); // removing "CELLS"
-            std::istringstream line_stream(current_line);
-            line_stream >> num_elems;
-
-            element_ids.resize(num_elems);
-            element_types.resize(num_elems);
-            element_connectivities.resize(num_elems);
-
-            CO_SIM_IO_INFO_IF("CoSimIO", GetEchoLevel()>1) << "Mesh contains " << num_elems << " Elements" << std::endl;
-
-            for (std::size_t i=0; i<num_elems; ++i) {
-                input_file >> num_nodes_per_elem;
-                element_connectivities[i].resize(num_nodes_per_elem);
-                for (std::size_t j=0; j<num_nodes_per_elem; ++j) {
-                    input_file >> element_connectivities[i][j];
-                }
-            }
-        }
-
-        // reading node Ids
-        if (current_line.find("NODE_ID") != std::string::npos) {
-            for (std::size_t i=0; i<nodal_ids.size(); ++i) { // nodal_ids was resized to correct size above
-                input_file >> nodal_ids[i];
-            }
-        }
-
-        // reading element Ids
-        if (current_line.find("ELEMENT_ID") != std::string::npos) {
-            for (std::size_t i=0; i<element_ids.size(); ++i) { // element_ids was resized to correct size above
-                input_file >> element_ids[i];
-            }
-        }
-
-        // reading element types
-        if (current_line.find("ELEMENT_TYPE") != std::string::npos) {
-            int enum_temp;
-            for (std::size_t i=0; i<element_types.size(); ++i) { // element_types was resized to correct size above
-                input_file >> enum_temp; // using a temp variable as enums cannot be read directly
-                element_types[i] = static_cast<CoSimIO::ElementType>(enum_temp);
-            }
-        }
-    }
-
-    // filling ModelPart with read information
-    for (std::size_t i=0; i<nodal_ids.size(); ++i) {
-        O_ModelPart.CreateNewNode(
-            nodal_ids[i],
-            nodal_coords[i*3],
-            nodal_coords[i*3+1],
-            nodal_coords[i*3+2]);
-    }
-    for (std::size_t i=0; i<element_ids.size(); ++i) {
-        for (auto& conn : element_connectivities[i]) {
-            conn = nodal_ids[conn]; // transforming vtk Ids back to original Ids
-        }
-        O_ModelPart.CreateNewElement(
-            element_ids[i],
-            element_types[i],
-            element_connectivities[i]);
-    }
-
-    input_file.close();
     RemovePath(file_name);
 
     CO_SIM_IO_INFO_IF("CoSimIO", GetEchoLevel()>1) << "Finished importing mesh" << std::endl;
@@ -376,86 +267,8 @@ Info FileCommunication::ExportMeshImpl(
 
     const auto start_time(std::chrono::steady_clock::now());
 
-    std::ofstream output_file;
-    output_file.open(GetTempFileName(file_name));
-    CheckStream(output_file, file_name);
+    SerializeToFile(GetTempFileName(file_name), "model_part", I_ModelPart, Serializer::TraceType::SERIALIZER_NO_TRACE);
 
-    output_file << std::scientific << std::setprecision(7); // TODO maybe this should be configurable
-
-    // write file header
-    output_file << "# vtk DataFile Version 4.0\n";
-    output_file << "CoSimIO FileCommunication\n";
-    output_file << "ASCII\n";
-    output_file << "DATASET UNSTRUCTURED_GRID\n\n";
-
-    // write nodes and create Id map
-    std::unordered_map<IdType, IdType> id_map;
-    IdType vtk_id = 0;
-    output_file << "POINTS " << I_ModelPart.NumberOfNodes() << " float\n";
-    for (auto node_it=I_ModelPart.NodesBegin(); node_it!=I_ModelPart.NodesEnd(); ++node_it) {
-        output_file << (*node_it)->X() << " " << (*node_it)->Y() << " " << (*node_it)->Z() << "\n";
-        id_map[(*node_it)->Id()] = vtk_id++;
-    }
-    output_file << "\n";
-
-    // get cells size information
-    std::size_t cell_list_size = 0;
-    for (auto elem_it=I_ModelPart.ElementsBegin(); elem_it!=I_ModelPart.ElementsEnd(); ++elem_it) {
-        cell_list_size += (*elem_it)->NumberOfNodes() + 1; // +1 for size of connectivity
-    }
-
-    // write cells connectivity
-    const auto const_id_map = id_map; // const reference to not accidentially modify the map
-    output_file << "CELLS " << I_ModelPart.NumberOfElements() << " " << cell_list_size << "\n";
-    for (auto elem_it=I_ModelPart.ElementsBegin(); elem_it!=I_ModelPart.ElementsEnd(); ++elem_it) {
-        const std::size_t num_nodes_cell = (*elem_it)->NumberOfNodes();
-        output_file << num_nodes_cell << " ";
-        std::size_t node_counter = 0;
-        for (auto node_it=(*elem_it)->NodesBegin(); node_it!=(*elem_it)->NodesEnd(); ++node_it) {
-            const IdType node_id = (*node_it)->Id();
-            auto id_iter = const_id_map.find(node_id);
-            CO_SIM_IO_ERROR_IF(id_iter == const_id_map.end()) << "The node with Id " << node_id << " is not part of the ModelPart but used for Element with Id " << (*elem_it)->Id() << std::endl;
-            output_file << id_iter->second;
-            if (node_counter++<num_nodes_cell-1) output_file << " "; // not adding a whitespace after last number
-        }
-        output_file << "\n";
-    }
-    output_file << "\n";
-
-    // write cell types
-    output_file << "CELL_TYPES " << I_ModelPart.NumberOfElements() << "\n";
-    for (auto elem_it=I_ModelPart.ElementsBegin(); elem_it!=I_ModelPart.ElementsEnd(); ++elem_it) {
-        output_file << static_cast<int>(GetVtkCellTypeForElementType((*elem_it)->Type())) << "\n";
-    }
-    output_file << "\n";
-
-    // writing node Ids
-    output_file << "POINT_DATA " << I_ModelPart.NumberOfNodes() << "\n";
-    output_file << "FIELD FieldData 1" << "\n";
-    output_file << "NODE_ID 1 " << I_ModelPart.NumberOfNodes() << " int\n";
-    for (auto node_it=I_ModelPart.NodesBegin(); node_it!=I_ModelPart.NodesEnd(); ++node_it) {
-        output_file << (*node_it)->Id() << "\n";
-    }
-    output_file << "\n";
-
-    // writing element Ids
-    output_file << "CELL_DATA " << I_ModelPart.NumberOfElements() << "\n";
-    output_file << "FIELD FieldData 1" << "\n";
-    output_file << "ELEMENT_ID 1 " << I_ModelPart.NumberOfElements() << " int\n";
-    for (auto elem_it=I_ModelPart.ElementsBegin(); elem_it!=I_ModelPart.ElementsEnd(); ++elem_it) {
-        output_file << (*elem_it)->Id() << "\n";
-    }
-    output_file << "\n";
-
-    // writing element types
-    output_file << "CELL_DATA " << I_ModelPart.NumberOfElements() << "\n";
-    output_file << "FIELD FieldData 1" << "\n";
-    output_file << "ELEMENT_TYPE 1 " << I_ModelPart.NumberOfElements() << " int\n";
-    for (auto elem_it=I_ModelPart.ElementsBegin(); elem_it!=I_ModelPart.ElementsEnd(); ++elem_it) {
-        output_file << static_cast<int>((*elem_it)->Type()) << "\n";
-    }
-
-    output_file.close();
     MakeFileVisible(file_name);
 
     CO_SIM_IO_INFO_IF("CoSimIO", GetEchoLevel()>1) << "Finished exporting mesh" << std::endl;
