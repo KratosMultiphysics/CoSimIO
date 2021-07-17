8c12609dea453ba2bb5e19ad203640c5c8f50d10
philbucher <philipp.bucher@tum.de>
2021-07-17 11:56:50 +0200

    more tests for DataCommunicator

--- tests/co_sim_io/impl/test_data_communicator.cpp
+++ tests/co_sim_io/impl/test_data_communicator.cpp
@@ -59,6 +59,413 @@ TEST_CASE("DataCommunicator_ErrorBroadcasting")
     CHECK_UNARY_FALSE(serial_communicator.ErrorIfFalseOnAnyRank(false));
 }
 
+TEST_CASE("DataCommunicator_sum_int")
+{
+    DataCommunicator serial_communicator;
+    constexpr int root = 0;
+
+    int local = 1;
+    int result = serial_communicator.Sum(local, root);
+    CHECK_EQ(result, local);
+}
+
+TEST_CASE("DataCommunicator_sum_double")
+{
+    DataCommunicator serial_communicator;
+    constexpr int root = 0;
+
+    double local = 1.58;
+    double result = serial_communicator.Sum(local, root);
+    CHECK_EQ(result, local);
+}
+
+TEST_CASE("DataCommunicator_sum_vec_int")
+{
+    DataCommunicator serial_communicator;
+    constexpr int root = 0;
+
+    std::vector<int> local{1, 1};
+    std::vector<int> output(local.size());
+
+    // two-buffer version
+    serial_communicator.Sum(local, output, root);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(output, local)
+
+    // return buffer version
+    std::vector<int> returned_result = serial_communicator.Sum(local, root);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(returned_result, local)
+
+    #ifdef CO_SIM_IO_DEBUG
+    std::vector<int> wrong_size_global{-1};
+    CHECK_THROWS_WITH(
+        serial_communicator.Sum(local, wrong_size_global, root),
+        "Error: Input error in call to DataCommunicator::Sum: The sizes of the local and distributed buffers do not match.\n"
+    );
+    #endif
+}
+
+TEST_CASE("DataCommunicator_sum_vec_double")
+{
+    DataCommunicator serial_communicator;
+    constexpr int root = 0;
+
+    std::vector<double> local{1.2, 1.87};
+    std::vector<double> output(local.size());
+
+    // two-buffer version
+    serial_communicator.Sum(local, output, root);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(output, local)
+
+    // return buffer version
+    std::vector<double> returned_result = serial_communicator.Sum(local, root);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(returned_result, local)
+
+    #ifdef CO_SIM_IO_DEBUG
+    std::vector<double> wrong_size_global{-1};
+    CHECK_THROWS_WITH(
+        serial_communicator.Sum(local, wrong_size_global, root),
+        "Error: Input error in call to DataCommunicator::Sum: The sizes of the local and distributed buffers do not match.\n"
+    );
+    #endif
+}
+
+TEST_CASE("DataCommunicator_min_int")
+{
+    DataCommunicator serial_communicator;
+    constexpr int root = 0;
+
+    int local = 1;
+    int result = serial_communicator.Min(local, root);
+    CHECK_EQ(result, local);
+}
+
+TEST_CASE("DataCommunicator_min_double")
+{
+    DataCommunicator serial_communicator;
+    constexpr int root = 0;
+
+    double local = 1.58;
+    double result = serial_communicator.Min(local, root);
+    CHECK_EQ(result, local);
+}
+
+TEST_CASE("DataCommunicator_min_vec_int")
+{
+    DataCommunicator serial_communicator;
+    constexpr int root = 0;
+
+    std::vector<int> local{1, 1};
+    std::vector<int> output(local.size());
+
+    // two-buffer version
+    serial_communicator.Min(local, output, root);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(output, local)
+
+    // return buffer version
+    std::vector<int> returned_result = serial_communicator.Min(local, root);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(returned_result, local)
+
+    #ifdef CO_SIM_IO_DEBUG
+    std::vector<int> wrong_size_global{-1};
+    CHECK_THROWS_WITH(
+        serial_communicator.Min(local, wrong_size_global, root),
+        "Error: Input error in call to DataCommunicator::Min: The sizes of the local and distributed buffers do not match.\n"
+    );
+    #endif
+}
+
+TEST_CASE("DataCommunicator_min_vec_double")
+{
+    DataCommunicator serial_communicator;
+    constexpr int root = 0;
+
+    std::vector<double> local{1.2, 1.87};
+    std::vector<double> output(local.size());
+
+    // two-buffer version
+    serial_communicator.Min(local, output, root);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(output, local)
+
+    // return buffer version
+    std::vector<double> returned_result = serial_communicator.Min(local, root);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(returned_result, local)
+
+    #ifdef CO_SIM_IO_DEBUG
+    std::vector<double> wrong_size_global{-1};
+    CHECK_THROWS_WITH(
+        serial_communicator.Min(local, wrong_size_global, root),
+        "Error: Input error in call to DataCommunicator::Min: The sizes of the local and distributed buffers do not match.\n"
+    );
+    #endif
+}
+
+TEST_CASE("DataCommunicator_max_int")
+{
+    DataCommunicator serial_communicator;
+    constexpr int root = 0;
+
+    int local = 1;
+    int result = serial_communicator.Max(local, root);
+    CHECK_EQ(result, local);
+}
+
+TEST_CASE("DataCommunicator_max_double")
+{
+    DataCommunicator serial_communicator;
+    constexpr int root = 0;
+
+    double local = 1.58;
+    double result = serial_communicator.Max(local, root);
+    CHECK_EQ(result, local);
+}
+
+TEST_CASE("DataCommunicator_max_vec_int")
+{
+    DataCommunicator serial_communicator;
+    constexpr int root = 0;
+
+    std::vector<int> local{1, 1};
+    std::vector<int> output(local.size());
+
+    // two-buffer version
+    serial_communicator.Max(local, output, root);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(output, local)
+
+    // return buffer version
+    std::vector<int> returned_result = serial_communicator.Max(local, root);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(returned_result, local)
+
+    #ifdef CO_SIM_IO_DEBUG
+    std::vector<int> wrong_size_global{-1};
+    CHECK_THROWS_WITH(
+        serial_communicator.Max(local, wrong_size_global, root),
+        "Error: Input error in call to DataCommunicator::Max: The sizes of the local and distributed buffers do not match.\n"
+    );
+    #endif
+}
+
+TEST_CASE("DataCommunicator_max_vec_double")
+{
+    DataCommunicator serial_communicator;
+    constexpr int root = 0;
+
+    std::vector<double> local{1.2, 1.87};
+    std::vector<double> output(local.size());
+
+    // two-buffer version
+    serial_communicator.Max(local, output, root);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(output, local)
+
+    // return buffer version
+    std::vector<double> returned_result = serial_communicator.Max(local, root);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(returned_result, local)
+
+    #ifdef CO_SIM_IO_DEBUG
+    std::vector<double> wrong_size_global{-1};
+    CHECK_THROWS_WITH(
+        serial_communicator.Max(local, wrong_size_global, root),
+        "Error: Input error in call to DataCommunicator::Max: The sizes of the local and distributed buffers do not match.\n"
+    );
+    #endif
+}
+
+TEST_CASE("DataCommunicator_sum_all_int")
+{
+    DataCommunicator serial_communicator;
+    int local = 1;
+    int result = serial_communicator.SumAll(local);
+    CHECK_EQ(result, local);
+}
+
+TEST_CASE("DataCommunicator_sum_all_double")
+{
+    DataCommunicator serial_communicator;
+
+    double local = 1.58;
+    double result = serial_communicator.SumAll(local);
+    CHECK_EQ(result, local);
+}
+
+TEST_CASE("DataCommunicator_sum_all_vec_int")
+{
+    DataCommunicator serial_communicator;
+
+    std::vector<int> local{1, 1};
+    std::vector<int> output(local.size());
+
+    // two-buffer version
+    serial_communicator.SumAll(local, output);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(output, local)
+
+    // return buffer version
+    std::vector<int> returned_result = serial_communicator.SumAll(local);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(returned_result, local)
+
+    #ifdef CO_SIM_IO_DEBUG
+    std::vector<int> wrong_size_global{-1};
+    CHECK_THROWS_WITH(
+        serial_communicator.SumAll(local, wrong_size_global),
+        "Error: Input error in call to DataCommunicator::SumAll: The sizes of the local and distributed buffers do not match.\n"
+    );
+    #endif
+}
+
+TEST_CASE("DataCommunicator_sum_all_vec_double")
+{
+    DataCommunicator serial_communicator;
+
+    std::vector<double> local{1.2, 1.87};
+    std::vector<double> output(local.size());
+
+    // two-buffer version
+    serial_communicator.SumAll(local, output);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(output, local)
+
+    // return buffer version
+    std::vector<double> returned_result = serial_communicator.SumAll(local);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(returned_result, local)
+
+    #ifdef CO_SIM_IO_DEBUG
+    std::vector<double> wrong_size_global{-1};
+    CHECK_THROWS_WITH(
+        serial_communicator.SumAll(local, wrong_size_global),
+        "Error: Input error in call to DataCommunicator::SumAll: The sizes of the local and distributed buffers do not match.\n"
+    );
+    #endif
+}
+
+TEST_CASE("DataCommunicator_min_all_int")
+{
+    DataCommunicator serial_communicator;
+
+    int local = 1;
+    int result = serial_communicator.MinAll(local);
+    CHECK_EQ(result, local);
+}
+
+TEST_CASE("DataCommunicator_min_all_double")
+{
+    DataCommunicator serial_communicator;
+
+    double local = 1.58;
+    double result = serial_communicator.MinAll(local);
+    CHECK_EQ(result, local);
+}
+
+TEST_CASE("DataCommunicator_min_all_vec_int")
+{
+    DataCommunicator serial_communicator;
+
+    std::vector<int> local{1, 1};
+    std::vector<int> output(local.size());
+
+    // two-buffer version
+    serial_communicator.MinAll(local, output);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(output, local)
+
+    // return buffer version
+    std::vector<int> returned_result = serial_communicator.MinAll(local);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(returned_result, local)
+
+    #ifdef CO_SIM_IO_DEBUG
+    std::vector<int> wrong_size_global{-1};
+    CHECK_THROWS_WITH(
+        serial_communicator.MinAll(local, wrong_size_global),
+        "Error: Input error in call to DataCommunicator::MinAll: The sizes of the local and distributed buffers do not match.\n"
+    );
+    #endif
+}
+
+TEST_CASE("DataCommunicator_min_all_vec_double")
+{
+    DataCommunicator serial_communicator;
+
+    std::vector<double> local{1.2, 1.87};
+    std::vector<double> output(local.size());
+
+    // two-buffer version
+    serial_communicator.MinAll(local, output);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(output, local)
+
+    // return buffer version
+    std::vector<double> returned_result = serial_communicator.MinAll(local);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(returned_result, local)
+
+    #ifdef CO_SIM_IO_DEBUG
+    std::vector<double> wrong_size_global{-1};
+    CHECK_THROWS_WITH(
+        serial_communicator.MinAll(local, wrong_size_global),
+        "Error: Input error in call to DataCommunicator::MinAll: The sizes of the local and distributed buffers do not match.\n"
+    );
+    #endif
+}
+
+TEST_CASE("DataCommunicator_max_all_int")
+{
+    DataCommunicator serial_communicator;
+
+    int local = 1;
+    int result = serial_communicator.MaxAll(local);
+    CHECK_EQ(result, local);
+}
+
+TEST_CASE("DataCommunicator_max_all_double")
+{
+    DataCommunicator serial_communicator;
+
+    double local = 1.58;
+    double result = serial_communicator.MaxAll(local);
+    CHECK_EQ(result, local);
+}
+
+TEST_CASE("DataCommunicator_max_all_vec_int")
+{
+    DataCommunicator serial_communicator;
+
+    std::vector<int> local{1, 1};
+    std::vector<int> output(local.size());
+
+    // two-buffer version
+    serial_communicator.MaxAll(local, output);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(output, local)
+
+    // return buffer version
+    std::vector<int> returned_result = serial_communicator.MaxAll(local);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(returned_result, local)
+
+    #ifdef CO_SIM_IO_DEBUG
+    std::vector<int> wrong_size_global{-1};
+    CHECK_THROWS_WITH(
+        serial_communicator.MaxAll(local, wrong_size_global),
+        "Error: Input error in call to DataCommunicator::MaxAll: The sizes of the local and distributed buffers do not match.\n"
+    );
+    #endif
+}
+
+TEST_CASE("DataCommunicator_max_all_vec_double")
+{
+    DataCommunicator serial_communicator;
+
+    std::vector<double> local{1.2, 1.87};
+    std::vector<double> output(local.size());
+
+    // two-buffer version
+    serial_communicator.MaxAll(local, output);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(output, local)
+
+    // return buffer version
+    std::vector<double> returned_result = serial_communicator.MaxAll(local);
+    CO_SIM_IO_CHECK_VECTOR_EQUAL(returned_result, local)
+
+    #ifdef CO_SIM_IO_DEBUG
+    std::vector<double> wrong_size_global{-1};
+    CHECK_THROWS_WITH(
+        serial_communicator.MaxAll(local, wrong_size_global),
+        "Error: Input error in call to DataCommunicator::MaxAll: The sizes of the local and distributed buffers do not match.\n"
+    );
+    #endif
+}
+
 TEST_CASE("DataCommunicator_serial_distributed_tests")
 {
     DataCommunicator serial_communicator;
