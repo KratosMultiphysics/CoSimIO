c87b3aabb6d25f84496895a021f14e512435e2a7
philbucher <philipp.bucher@tum.de>
2021-07-22 17:35:13 +0200

    started adding tests

--- tests/co_sim_io/cpp/test_info.cpp
+++ tests/co_sim_io/cpp/test_info.cpp
@@ -90,6 +90,21 @@ TEST_CASE("info_basics_char")
     CHECK_EQ(info.Get<std::string>("identifier"), "pressure"); // char* will be turned into string
 }
 
+TEST_CASE("info_basics_info")
+{
+    Info info;
+
+    CHECK_UNARY_FALSE(info.Has("identifier"));
+
+    Info sub_info;
+
+    info.Set<Info>("identifier", sub_info);
+
+    CHECK_UNARY(info.Has("identifier"));
+
+    CHECK_EQ(info.Get<Info>("identifier").Size(), 0);
+}
+
 TEST_CASE("info_int_default")
 {
     Info info;
@@ -131,6 +146,19 @@ TEST_CASE("info_string_default")
     CHECK_UNARY_FALSE(info.Has("identifier"));
 
     CHECK_EQ(info.Get<std::string>("identifier", "dummy"), "dummy");
+
+    CHECK_UNARY_FALSE(info.Has("identifier")); // getting the default must not insert it!
+}
+
+TEST_CASE("info_info_default")
+{
+    Info info;
+
+    Info other_info;
+
+    CHECK_UNARY_FALSE(info.Has("identifier"));
+
+    CHECK_EQ(info.Get<Info>("identifier", other_info).Size(), 0);
 
     CHECK_UNARY_FALSE(info.Has("identifier")); // getting the default must not insert it!
 }
@@ -179,6 +207,80 @@ TEST_CASE("info_many_values")
     CHECK_UNARY(info.Get<bool>("is_converged"));
     CHECK_EQ(info.Get<double>("tol"), doctest::Approx(0.008));
     CHECK_EQ(info.Get<int>("echo_level"), 2);
+}
+
+TEST_CASE("info_many_values_nested")
+{
+    Info info;
+    Info sub_info;
+    Info sub_sub_info;
+
+    CHECK_UNARY_FALSE(info.Has("identifier"));
+    CHECK_UNARY_FALSE(info.Has("is_converged"));
+    CHECK_UNARY_FALSE(info.Has("tol"));
+    CHECK_UNARY_FALSE(info.Has("echo_level"));
+
+    info.Set<std::string>("identifier", "velocity_interface");
+    info.Set<bool>("is_converged", true);
+    info.Set<double>("tol", 0.008);
+    info.Set<int>("echo_level", 2);
+
+    sub_info.Set<std::string>("abc", "something");
+    sub_info.Set<bool>("minor", false);
+    sub_info.Set<double>("abs", -78.8);
+    sub_info.Set<int>("verb", 8);
+
+    sub_sub_info.Set<std::string>("the_key", "diff");
+    sub_sub_info.Set<bool>("hello", true);
+    sub_sub_info.Set<double>("value", 1.887e4);
+    sub_sub_info.Set<int>("id", 8189);
+
+    CHECK_UNARY(info.Has("identifier"));
+    CHECK_UNARY(info.Has("is_converged"));
+    CHECK_UNARY(info.Has("tol"));
+    CHECK_UNARY(info.Has("echo_level"));
+
+    CHECK_UNARY(sub_info.Has("abc"));
+    CHECK_UNARY(sub_info.Has("minor"));
+    CHECK_UNARY(sub_info.Has("abs"));
+    CHECK_UNARY(sub_info.Has("verb"));
+
+    CHECK_UNARY(sub_sub_info.Has("the_key"));
+    CHECK_UNARY(sub_sub_info.Has("hello"));
+    CHECK_UNARY(sub_sub_info.Has("value"));
+    CHECK_UNARY(sub_sub_info.Has("id"));
+
+    sub_info.Set<Info>("sub_sub", sub_sub_info);
+    info.Set<Info>("sub", sub_info);
+
+    CHECK_EQ(info.Get<std::string>("identifier"), "velocity_interface");
+    CHECK_UNARY(info.Get<bool>("is_converged"));
+    CHECK_EQ(info.Get<double>("tol"), doctest::Approx(0.008));
+    CHECK_EQ(info.Get<int>("echo_level"), 2);
+
+    Info ret_sub_info = info.Get<Info>("sub");
+
+    CHECK_UNARY(ret_sub_info.Has("abc"));
+    CHECK_UNARY(ret_sub_info.Has("minor"));
+    CHECK_UNARY(ret_sub_info.Has("abs"));
+    CHECK_UNARY(ret_sub_info.Has("verb"));
+
+    CHECK_EQ(ret_sub_info.Get<std::string>("abc"), "something");
+    CHECK_UNARY_FALSE(ret_sub_info.Get<bool>("minor"));
+    CHECK_EQ(ret_sub_info.Get<double>("abs"), doctest::Approx(-78.8));
+    CHECK_EQ(ret_sub_info.Get<int>("verb"), 8);
+
+    Info ret_sub_sub_info = ret_sub_info.Get<Info>("sub_sub");
+
+    CHECK_UNARY(ret_sub_sub_info.Has("the_key"));
+    CHECK_UNARY(ret_sub_sub_info.Has("hello"));
+    CHECK_UNARY(ret_sub_sub_info.Has("value"));
+    CHECK_UNARY(ret_sub_sub_info.Has("id"));
+
+    CHECK_EQ(ret_sub_sub_info.Get<std::string>("the_key"), "diff");
+    CHECK_UNARY(ret_sub_sub_info.Get<bool>("hello"));
+    CHECK_EQ(ret_sub_sub_info.Get<double>("value"), doctest::Approx(18870.0));
+    CHECK_EQ(ret_sub_sub_info.Get<int>("id"), 8189);
 }
 
 TEST_CASE("info_set_alreay_existing")
