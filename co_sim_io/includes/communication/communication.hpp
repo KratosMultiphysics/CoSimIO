ccb7df869b39e54a7a70b712e1d5132a6f8cca0c
philbucher <philipp.bucher@tum.de>
2021-08-05 21:06:31 +0200

    minor cleanup in Communication

--- co_sim_io/includes/communication/communication.hpp
+++ co_sim_io/includes/communication/communication.hpp
@@ -78,12 +78,13 @@ public:
     }
 
 protected:
-    std::string GetConnectionName() const {return mConnectionName;}
-    fs::path GetWorkingDirectory() const  {return mWorkingDirectory;}
-    int GetEchoLevel() const              {return mEchoLevel;}
-    bool GetIsPrimaryConnection() const   {return mIsPrimaryConnection;}
-    bool GetPrintTiming() const           {return mPrintTiming;}
-    bool GetIsConnected() const           {return mIsConnected;}
+    std::string GetConnectionName() const      {return mConnectionName;}
+    fs::path GetWorkingDirectory() const       {return mWorkingDirectory;}
+    fs::path GetCommunicationDirectory() const {return mCommFolder;}
+    int GetEchoLevel() const                   {return mEchoLevel;}
+    bool GetIsPrimaryConnection() const        {return mIsPrimaryConnection;}
+    bool GetPrintTiming() const                {return mPrintTiming;}
+    bool GetIsConnected() const                {return mIsConnected;}
 
     fs::path GetTempFileName(const fs::path& rPath) const;
 
