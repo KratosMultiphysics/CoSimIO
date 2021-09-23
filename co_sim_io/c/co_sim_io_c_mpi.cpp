d88ca82a1293c1bfb00b01d4ace7055bd8e116e5
Philipp <philipp.bucher@tum.de>
2021-09-23 15:57:47 +0200

    initial add C MPI interface

---
+++ co_sim_io/c/co_sim_io_c_mpi.cpp
@@ -0,0 +1,19 @@
+//     ______     _____ _           ________
+//    / ____/___ / ___/(_)___ ___  /  _/ __ |
+//   / /   / __ \\__ \/ / __ `__ \ / // / / /
+//  / /___/ /_/ /__/ / / / / / / // // /_/ /
+//  \____/\____/____/_/_/ /_/ /_/___/\____/
+//  Kratos CoSimulationApplication
+//
+//  License:         BSD License, see license.txt
+//
+//  Main authors:    Philipp Bucher (https://github.com/philbucher)
+//
+
+// Project includes
+extern "C" {
+#include "co_sim_io_c_mpi.h"
+}
+#include "co_sim_io_mpi.hpp"
+
+
