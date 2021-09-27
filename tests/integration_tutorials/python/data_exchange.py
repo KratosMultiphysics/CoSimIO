15637455c8becd6de9beaa459bea904a4bf74994
Philipp Bucher <philipp.bucher@tum.de>
2021-09-27 21:20:22 +0200

    completing python tutorials (#287)
    * updated main page
    
    * rename old files
    
    * WIP python docs
    
    * minor improvements in C++ docs
    
    * updated first py tutorial
    
    * improved C++ tutorials
    
    * minor
    
    * Py tutorial data exchange with Kratos
    
    * removed old file
    
    * missing code
    
    * adding python mapping tutorial
    
    * removed old code
    
    * python tutorial for mesh exchange

--- tests/integration_tutorials/python/data_exchange.py
+++ tests/integration_tutorials/python/data_exchange.py
@@ -7,7 +7,7 @@
 #
 #  License:         BSD License, see license.txt
 #
-#  Main authors:    Pooyan Dadvand
+#  Main authors:    Philipp Bucher (https://github.com/philbucher)
 #
 
 import CoSimIO
@@ -18,8 +18,8 @@ def cosimio_check_equal(a, b):
 
 # Connection Settings
 settings = CoSimIO.Info()
-settings.SetString("my_name", "py_import_data")
-settings.SetString("connect_to", "py_export_data")
+settings.SetString("my_name", "my_code")
+settings.SetString("connect_to", "Kratos")
 settings.SetInt("echo_level", 1)
 settings.SetString("version", "1.25")
 
@@ -28,17 +28,25 @@ return_info = CoSimIO.Connect(settings)
 cosimio_check_equal(return_info.GetInt("connection_status"), CoSimIO.ConnectionStatus.Connected)
 connection_name = return_info.GetString("connection_name")
 
-# Importing data
+# send data to Kratos
+data_to_be_send=CoSimIO.DoubleVector([3.14] * 4)
+info = CoSimIO.Info()
+info.SetString("identifier", "vector_of_pi")
+info.SetString("connection_name", connection_name)
+return_info = CoSimIO.ExportData(info, data_to_be_send)
+
+# receive the same data from Kratos
 info = CoSimIO.Info()
 info.SetString("identifier", "vector_of_pi")
 info.SetString("connection_name", connection_name)
 vec_to_import=CoSimIO.DoubleVector()
 return_info = CoSimIO.ImportData(info, vec_to_import)
 
-cosimio_check_equal(len(vec_to_import), 4)
+# check that the data is the same:
+cosimio_check_equal(len(vec_to_import), len(data_to_be_send))
 
-for value in vec_to_import:
-    cosimio_check_equal(value, 3.14)
+for value, val_ref in zip(vec_to_import, data_to_be_send):
+    cosimio_check_equal(value, val_ref)
 
 # Disconnecting
 disconnect_settings = CoSimIO.Info()
