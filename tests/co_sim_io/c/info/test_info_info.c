0351fbf310ca2af6a172c8b825c655a5a499e352
philbucher <philipp.bucher@tum.de>
2021-09-27 16:21:50 +0200

    added test

--- tests/co_sim_io/c/info/test_info_info.c
+++ tests/co_sim_io/c/info/test_info_info.c
@@ -19,34 +19,38 @@ int main()
 {
     /* declaring variables */
     CoSimIO_Info info;
+    CoSimIO_Info sub_info;
+    CoSimIO_Info retrieved_sub_info;
 
     info = CoSimIO_CreateInfo();
+    sub_info = CoSimIO_CreateInfo();
 
     COSIMIO_CHECK_INT_EQUAL(CoSimIO_Info_Size(info), 0);
+    COSIMIO_CHECK_INT_EQUAL(CoSimIO_Info_Size(sub_info), 0);
 
-    CoSimIO_Info_SetInt(info, "echo_level", 6);
-    CoSimIO_Info_SetString(info, "iter", "15");
-    CoSimIO_Info_SetInt(info, "error_code", 0);
-    CoSimIO_Info_SetBool(info, "is_converged", 1);
-    CoSimIO_Info_SetDouble(info, "tolerance", 0.00331);
+    CoSimIO_Info_SetInt(sub_info, "echo_level", 6);
+    CoSimIO_Info_SetString(sub_info, "iter", "15");
+    CoSimIO_Info_SetInt(sub_info, "error_code", 0);
+    CoSimIO_Info_SetBool(sub_info, "is_converged", 1);
+    CoSimIO_Info_SetDouble(sub_info, "tolerance", 0.00331);
 
-    COSIMIO_CHECK_TRUE(CoSimIO_Info_Has(info, "echo_level"));
-    COSIMIO_CHECK_TRUE(CoSimIO_Info_Has(info, "iter"));
-    COSIMIO_CHECK_TRUE(CoSimIO_Info_Has(info, "error_code"));
-    COSIMIO_CHECK_TRUE(CoSimIO_Info_Has(info, "is_converged"));
-    COSIMIO_CHECK_TRUE(CoSimIO_Info_Has(info, "tolerance"));
+    CoSimIO_Info_SetInfo(info, "sub_settings", sub_info);
 
-    return 1; // implement the test properly!
+    COSIMIO_CHECK_TRUE(CoSimIO_Info_Has(info, "sub_settings"));
 
-    COSIMIO_CHECK_INT_EQUAL(CoSimIO_Info_GetInt(info, "echo_level"), 6);
-    COSIMIO_CHECK_STRING_EQUAL(CoSimIO_Info_GetString(info, "iter"), "15");
-    COSIMIO_CHECK_INT_EQUAL(CoSimIO_Info_GetInt(info, "error_code"), 0);
-    COSIMIO_CHECK_TRUE(CoSimIO_Info_GetBool(info, "is_converged"));
-    COSIMIO_CHECK_DOUBLE_EQUAL(CoSimIO_Info_GetDouble(info, "tolerance"), 0.00331);
+    retrieved_sub_info = CoSimIO_Info_GetInfo(info, "sub_settings");
 
-    COSIMIO_CHECK_INT_EQUAL(CoSimIO_Info_Size(info), 5);
+    COSIMIO_CHECK_INT_EQUAL(CoSimIO_Info_GetInt(retrieved_sub_info, "echo_level"), 6);
+    COSIMIO_CHECK_STRING_EQUAL(CoSimIO_Info_GetString(retrieved_sub_info, "iter"), "15");
+    COSIMIO_CHECK_INT_EQUAL(CoSimIO_Info_GetInt(retrieved_sub_info, "error_code"), 0);
+    COSIMIO_CHECK_TRUE(CoSimIO_Info_GetBool(retrieved_sub_info, "is_converged"));
+    COSIMIO_CHECK_DOUBLE_EQUAL(CoSimIO_Info_GetDouble(retrieved_sub_info, "tolerance"), 0.00331);
+
+    COSIMIO_CHECK_INT_EQUAL(CoSimIO_Info_Size(retrieved_sub_info), 5);
 
     CoSimIO_FreeInfo(info);
+    CoSimIO_FreeInfo(sub_info);
+    CoSimIO_FreeInfo(retrieved_sub_info);
 
     return 0;
 }
