b3a150a08d08adc004af028b0e0b08faf9b2e0f7
Philipp <philipp.bucher@tum.de>
2021-09-29 16:51:33 +0200

    adding CO_SIM_IO_BUILD_PYTHON_MPI4PY_INTERFACE to script

--- scripts/build_python.sh
+++ scripts/build_python.sh
@@ -1,4 +1,8 @@
 # should be called with "sh scripts/build_python.sh" from the root-directory
 
-cmake -H"." -B"build" -DCO_SIM_IO_BUILD_PYTHON=ON -DBUILD_TESTING=ON -DCO_SIM_IO_ENABLE_MPI=OFF
+cmake -H"." -B"build" \
+    -DCO_SIM_IO_BUILD_PYTHON=ON \
+    -DBUILD_TESTING=ON \
+    -DCO_SIM_IO_ENABLE_MPI=OFF \
+    -DCO_SIM_IO_BUILD_PYTHON_MPI4PY_INTERFACE=OFF
 cmake --build "build" --target install -- -j$(nproc)
