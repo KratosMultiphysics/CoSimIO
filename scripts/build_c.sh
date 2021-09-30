a885fd768effc6d5ee4406fcd7328a4f5ba840ce
philbucher <philipp.bucher@tum.de>
2021-09-30 13:30:19 +0200

    missing updates

--- scripts/build_c.sh
+++ scripts/build_c.sh
@@ -1,4 +1,4 @@
 # should be called with "sh scripts/build_c.sh" from the root-directory
 
-cmake -H"." -B"build" -DCO_SIM_IO_BUILD_C=ON -DBUILD_TESTING=ON -DCO_SIM_IO_ENABLE_MPI=OFF
+cmake -H"." -B"build" -DCO_SIM_IO_BUILD_C=ON -DCO_SIM_IO_BUILD_TESTING=ON -DCO_SIM_IO_ENABLE_MPI=OFF
 cmake --build "build" --target install -- -j$(nproc)
--- scripts/build_cpp.sh
+++ scripts/build_cpp.sh
@@ -1,4 +1,4 @@
 # should be called with "sh scripts/build_cpp.sh" from the root-directory
 
-cmake -H"." -B"build" -DBUILD_TESTING=ON -DCO_SIM_IO_ENABLE_MPI=OFF
+cmake -H"." -B"build" -DCO_SIM_IO_BUILD_TESTING=ON -DCO_SIM_IO_ENABLE_MPI=OFF
 cmake --build "build" --target install -- -j$(nproc)
