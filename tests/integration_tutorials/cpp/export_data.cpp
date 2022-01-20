fcf5b07ae043044b08406a75f38de6256ee901d4
Philipp Bucher <philipp.bucher@tum.de>
2022-01-20 11:02:40 +0100

    using CoSimIO try/catch

--- tests/integration_tutorials/cpp/export_data.cpp
+++ tests/integration_tutorials/cpp/export_data.cpp
@@ -26,138 +26,137 @@
 
 int main()
 {
-    try {
-        namespace fs = ghc::filesystem;
+    CO_SIM_IO_TRY
 
-        fs::path res_dir("data_exchange_profiling");
+    namespace fs = ghc::filesystem;
 
-        fs::remove_all(res_dir);
-        fs::create_directory(res_dir);
+    fs::path res_dir("data_exchange_profiling");
 
-        bool header_written = false;
-        bool uses_serializer = false;
+    fs::remove_all(res_dir);
+    fs::create_directory(res_dir);
 
-        int counter = 0;
-        for (CoSimIO::Info config : GetTestingMatrix()) {
-            header_written = false;
-            uses_serializer = false;
+    bool header_written = false;
+    bool uses_serializer = false;
 
-            const std::string file_name = GetFileName(config);
+    int counter = 0;
+    for (CoSimIO::Info config : GetTestingMatrix()) {
+        header_written = false;
+        uses_serializer = false;
 
-            fs::path res_file_name = res_dir / fs::path(file_name);
+        const std::string file_name = GetFileName(config);
 
-            std::ofstream res_file;
-            res_file.open(res_file_name.string());
-            res_file << "# "; // comment for file
-            config.Print(res_file, "#");
+        fs::path res_file_name = res_dir / fs::path(file_name);
 
-            std::cout << "Current configuration (" << file_name << "):\n" << config << std::endl;
-            const std::string my_name = "cpp_export_solver_" + std::to_string(counter);
-            const std::string connect_to = "cpp_import_solver_" + std::to_string(counter);
-            counter++;
+        std::ofstream res_file;
+        res_file.open(res_file_name.string());
+        res_file << "# "; // comment for file
+        config.Print(res_file, "#");
 
-            config.Set("my_name", my_name);
-            config.Set("connect_to", connect_to);
-            // config.Set("echo_level", 2);
+        std::cout << "Current configuration (" << file_name << "):\n" << config << std::endl;
+        const std::string my_name = "cpp_export_solver_" + std::to_string(counter);
+        const std::string connect_to = "cpp_import_solver_" + std::to_string(counter);
+        counter++;
 
-            auto info = CoSimIO::Connect(config);
-            COSIMIO_CHECK_EQUAL(info.Get<int>("connection_status"), CoSimIO::ConnectionStatus::Connected);
-            const std::string connection_name = info.Get<std::string>("connection_name");
+        config.Set("my_name", my_name);
+        config.Set("connect_to", connect_to);
+        // config.Set("echo_level", 2);
 
-            std::vector<double> data_to_send(VEC_SIZES.back());
-            for (std::size_t vec_size : VEC_SIZES) {
+        auto info = CoSimIO::Connect(config);
+        COSIMIO_CHECK_EQUAL(info.Get<int>("connection_status"), CoSimIO::ConnectionStatus::Connected);
+        const std::string connection_name = info.Get<std::string>("connection_name");
 
-                const auto start_time(std::chrono::steady_clock::now());
+        std::vector<double> data_to_send(VEC_SIZES.back());
+        for (std::size_t vec_size : VEC_SIZES) {
 
-                data_to_send.resize(vec_size);
-                std::cout << "\nCurrent vector size: " << vec_size << std::endl;
+            const auto start_time(std::chrono::steady_clock::now());
 
-                double accum_time = 0.0;
-                std::size_t accum_mem = 0;
-                double accum_time_ipc = 0.0;
-                double accum_time_serializer = 0.0;
+            data_to_send.resize(vec_size);
+            std::cout << "\nCurrent vector size: " << vec_size << std::endl;
 
-                for (int i=0; i<NUM_EVALUATIONS+1; ++i) {
-                    info.Clear();
-                    info.Set("identifier", "vector_of_pi_1");
-                    info.Set("connection_name", connection_name);
-                    info = CoSimIO::ExportData(info, data_to_send);
+            double accum_time = 0.0;
+            std::size_t accum_mem = 0;
+            double accum_time_ipc = 0.0;
+            double accum_time_serializer = 0.0;
 
-                    if (i>0) {
-                        accum_time += info.Get<double>("elapsed_time");
-                        accum_mem  += info.Get<std::size_t>("memory_usage_ipc");
-                        if (info.Has("elapsed_time_ipc")) {
-                            uses_serializer = true;
-                            accum_time_ipc += info.Get<double>("elapsed_time_ipc");
-                            accum_time_serializer += info.Get<double>("elapsed_time_serializer");
-                        }
-                    }
+            for (int i=0; i<NUM_EVALUATIONS+1; ++i) {
+                info.Clear();
+                info.Set("identifier", "vector_of_pi_1");
+                info.Set("connection_name", connection_name);
+                info = CoSimIO::ExportData(info, data_to_send);
 
-                    info.Clear();
-                    info.Set("identifier", "vector_of_pi_2");
-                    info.Set("connection_name", connection_name);
-                    info = CoSimIO::ImportData(info, data_to_send);
-
-                    if (i>0) {
-                        accum_time += info.Get<double>("elapsed_time");
-                        accum_mem  += info.Get<std::size_t>("memory_usage_ipc");
-                        if (uses_serializer) {
-                            accum_time_ipc += info.Get<double>("elapsed_time_ipc");
-                            accum_time_serializer += info.Get<double>("elapsed_time_serializer");
-                        }
+                if (i>0) {
+                    accum_time += info.Get<double>("elapsed_time");
+                    accum_mem  += info.Get<std::size_t>("memory_usage_ipc");
+                    if (info.Has("elapsed_time_ipc")) {
+                        uses_serializer = true;
+                        accum_time_ipc += info.Get<double>("elapsed_time_ipc");
+                        accum_time_serializer += info.Get<double>("elapsed_time_serializer");
                     }
                 }
 
-                const double avg_time = accum_time/(NUM_EVALUATIONS*2);
-                const double avg_time_ipc = accum_time_ipc/(NUM_EVALUATIONS*2);
-                const double avg_time_serializer = accum_time_serializer/(NUM_EVALUATIONS*2);
-                const std::size_t avg_mem = accum_mem/(NUM_EVALUATIONS*2);
+                info.Clear();
+                info.Set("identifier", "vector_of_pi_2");
+                info.Set("connection_name", connection_name);
+                info = CoSimIO::ImportData(info, data_to_send);
 
-                std::cout << "    Elapsed time: " << avg_time << std::endl;
-                std::cout << "    Memory usage: " << HumanReadableSize(avg_mem) << std::endl;
-                std::cout << "    Speed: " << HumanReadableSize(avg_mem/avg_time) << "/s" << std::endl;
-
-                if (!header_written) {
-                    header_written = true;
-                    res_file << "\n# Vector-size  avg-time[s]  avg-memory[B]";
+                if (i>0) {
+                    accum_time += info.Get<double>("elapsed_time");
+                    accum_mem  += info.Get<std::size_t>("memory_usage_ipc");
                     if (uses_serializer) {
-                        res_file << " avg-time-ipc[s] avg-time-serializer[s]";
+                        accum_time_ipc += info.Get<double>("elapsed_time_ipc");
+                        accum_time_serializer += info.Get<double>("elapsed_time_serializer");
                     }
-                    res_file << "\n";
-                }
-
-                res_file << vec_size << " " << avg_time << " " << avg_mem;
-                if (uses_serializer) {
-                    res_file << " " << avg_time_ipc << " " << avg_time_serializer;
-                }
-                res_file << "\n";
-
-                const bool break_and_go_to_next_config = ElapsedSeconds(start_time) > 60;
-
-                CoSimIO::Info loop_info;
-                loop_info.Set("identifier", "loop_info");
-                loop_info.Set("connection_name", connection_name);
-                loop_info.Set("loop_info", break_and_go_to_next_config);
-
-                CoSimIO::ExportInfo(loop_info);
-
-                if (break_and_go_to_next_config) {
-                    std::cout << "BREAKING ..." << std::endl;
-                    break;
                 }
             }
 
-            CoSimIO::Info disconnect_settings;
-            disconnect_settings.Set("connection_name", connection_name);
-            info = CoSimIO::Disconnect(disconnect_settings); // disconnect afterwards
-            COSIMIO_CHECK_EQUAL(info.Get<int>("connection_status"), CoSimIO::ConnectionStatus::Disconnected);
-            std::cout << "\n" << std::endl;
+            const double avg_time = accum_time/(NUM_EVALUATIONS*2);
+            const double avg_time_ipc = accum_time_ipc/(NUM_EVALUATIONS*2);
+            const double avg_time_serializer = accum_time_serializer/(NUM_EVALUATIONS*2);
+            const std::size_t avg_mem = accum_mem/(NUM_EVALUATIONS*2);
 
-            res_file.close();
+            std::cout << "    Elapsed time: " << avg_time << std::endl;
+            std::cout << "    Memory usage: " << HumanReadableSize(avg_mem) << std::endl;
+            std::cout << "    Speed: " << HumanReadableSize(avg_mem/avg_time) << "/s" << std::endl;
+
+            if (!header_written) {
+                header_written = true;
+                res_file << "\n# Vector-size  avg-time[s]  avg-memory[B]";
+                if (uses_serializer) {
+                    res_file << " avg-time-ipc[s] avg-time-serializer[s]";
+                }
+                res_file << "\n";
+            }
+
+            res_file << vec_size << " " << avg_time << " " << avg_mem;
+            if (uses_serializer) {
+                res_file << " " << avg_time_ipc << " " << avg_time_serializer;
+            }
+            res_file << "\n";
+
+            const bool break_and_go_to_next_config = ElapsedSeconds(start_time) > 60;
+
+            CoSimIO::Info loop_info;
+            loop_info.Set("identifier", "loop_info");
+            loop_info.Set("connection_name", connection_name);
+            loop_info.Set("loop_info", break_and_go_to_next_config);
+
+            CoSimIO::ExportInfo(loop_info);
+
+            if (break_and_go_to_next_config) {
+                std::cout << "BREAKING ..." << std::endl;
+                break;
+            }
         }
-    }catch(CoSimIO::Internals::Exception& e)      {  std::cout << "CoSimIO exception occured: " << e.what() << std::endl; } \
-catch(std::exception& e) { std::cout << "std exception occured: " << e.what() << std::endl; }                   \
-catch(...)               { std::cout << "Unknown error"; }
 
+        CoSimIO::Info disconnect_settings;
+        disconnect_settings.Set("connection_name", connection_name);
+        info = CoSimIO::Disconnect(disconnect_settings); // disconnect afterwards
+        COSIMIO_CHECK_EQUAL(info.Get<int>("connection_status"), CoSimIO::ConnectionStatus::Disconnected);
+        std::cout << "\n" << std::endl;
+
+        res_file.close();
+    }
+
+    CO_SIM_IO_CATCH
     return 0;
 }
