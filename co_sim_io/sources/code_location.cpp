b1c3a7d287211888de0c6a8e53b72a23386a9b1d
Philipp <philipp.bucher@tum.de>
2023-09-04 23:27:34 +0200

    port changes

--- co_sim_io/sources/code_location.cpp
+++ co_sim_io/sources/code_location.cpp
@@ -21,8 +21,34 @@ namespace Internals {
 
 std::string CodeLocation::GetCleanFileName() const
 {
+#ifndef CO_SIM_IO_COMPILED_IN_WINDOWS
     return fs::canonical(fs::path(GetFileName())).lexically_relative(fs::absolute(".")).string();
+#else
+    // NOTE: This is a workaround for the above, since it doesn't work on some Windows systems. Inspired in Kratos solution https://github.com/KratosMultiphysics/Kratos/blob/c1d9b2e30b557612aa379acfa2e16298523731a8/kratos/sources/code_location.cpp#L46
+    std::string clean_file_name(GetFileName());
+
+    // Replace all backslashes with forward slashes
+    std::size_t start_position = 0;
+    const std::string from_string = "\\";
+    const std::string to_string = "/";
+    while ((start_position = clean_file_name.find(from_string, start_position)) != std::string::npos) {
+        clean_file_name.replace(start_position, from_string.length(), to_string);
+        start_position += to_string.length();
+    }
+
+    // Remove the path up to the co_sim_io root folder
+    std::size_t co_sim_io_root_position = clean_file_name.rfind("/co_sim_io/");
+    if (co_sim_io_root_position != std::string::npos) {
+        clean_file_name.erase(0, co_sim_io_root_position+1);
+        return clean_file_name;
+    }
+    if (co_sim_io_root_position != std::string::npos)
+        clean_file_name.erase(0, co_sim_io_root_position + 1 );
+
+    // Return the file name
+    return clean_file_name;
+#endif
 }
 
-} // namespace Interals
+} // namespace Internals
 } // namespace CoSimIO
